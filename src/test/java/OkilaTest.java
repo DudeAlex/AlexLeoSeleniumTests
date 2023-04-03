ac411fe375ee0d8ffa2cdc10ede2acfc4889b06f
okila <37234241+okila-ok@users.noreply.github.com>
2023-04-03 16:04:29 -0700

    Okila (#4)
     - created method to check Goggle page accessibility

--- src/test/java/OkilaTest.java
+++ src/test/java/OkilaTest.java
@@ -1,8 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class OkilaTest {
 
     @Test
-    public void doFirstTest(){
 
+    public void doFirstTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.quit();
     }
+}
+

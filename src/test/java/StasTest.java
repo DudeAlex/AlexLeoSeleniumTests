851a69bf5ac308e3ad1f45c42a2bee5836a37568
thestaskalinin <thestaskalinin@gmail.com>
2023-04-05 10:19:23 -0400

    Minor change

---
+++ src/test/java/StasTest.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class StasTest {
+
+
+    @Test
+    WebDriver driver  = new ChromeDriver();
+
+}

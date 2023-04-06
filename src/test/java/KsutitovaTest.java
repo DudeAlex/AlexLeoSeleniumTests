05f7b049f29c47e85303f7b5dbeb5670d36ee2b1
ktitova <ksunchik123@yandex.ru>
2023-04-06 08:15:59 +0400

    VerifyFiveCardTest

--- src/test/java/KsutitovaTest.java
+++ src/test/java/KsutitovaTest.java
@@ -1,10 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class KsutitovaTest {
+import java.util.List;
+
+
+public class KsutitovaTest<String> {
 
     @Test
     public void verifyTheDiscountMessageTest() {
@@ -12,9 +16,42 @@ public class KsutitovaTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://askomdch.com/");
-        String discountMessage = driver.findElement
+
+        String discountMessage = (String) driver.findElement
                 (By.xpath("//div[@class='wp-block-column is-vertically-aligned-center']/h3")).getText();
         Assert.assertEquals(discountMessage, "25% OFF On all products");
+
         driver.quit();
     }
+
+
+    @Test
+    public void clickAccountButtonTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://askomdch.com/");
+
+        driver.findElement(By.xpath("//li[@id='menu-item-1237']/a")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/account/");
+
+        driver.quit();
+    }
+
+    @Test
+    public void VerifyFiveCardTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://askomdch.com/");
+
+        List<WebElement> rezult = driver.findElements(By.xpath("//div[@class='astra-shop-summary-wrap']"));
+        Assert.assertEquals(rezult.size(), 5);
+
+        driver.quit();
+    }
+
 }
+
+
+

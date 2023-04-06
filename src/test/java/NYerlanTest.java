b430c5feb3cee98bda533a5753cefadd019cb8af
nyerlan <n.yerlan@gmail.com>
2023-04-05 21:24:54 -0700

    TC_003.01 - added Before and After Method

--- src/test/java/NYerlanTest.java
+++ src/test/java/NYerlanTest.java
@@ -3,19 +3,33 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class NYerlanTest {
 
     private final String SITE_NAME = "AskOmDch – Become a Selenium automation expert!";
     private final String DISCOUNT_INFO = "25% OFF On all products";
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+        driver.manage().window().maximize();
+        driver.get("https://askomdch.com/");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        driver.quit();
+    }
 
     @Test
     public void testAssertSiteTitle() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com/");
-        Thread.sleep(2000);
 
         Assert.assertEquals(driver.getTitle(), SITE_NAME);
 
@@ -23,16 +37,12 @@ public class NYerlanTest {
     }
 
     @Test
-    public void testAssertDiscountInfo() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com/");
+    public void testAssertDiscountInfo_TC_003_01() throws InterruptedException {
 
         WebElement textDiscount = driver.findElement(By.xpath("//h3[text()='25% OFF On all products']"));
 
         Assert.assertEquals(textDiscount.getText(), DISCOUNT_INFO);
 
-        Thread.sleep(2000);
         driver.quit();
     }
 }

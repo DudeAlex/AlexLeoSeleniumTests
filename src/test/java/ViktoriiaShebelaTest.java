09ddc5bfbfc7ddf25fa9da92745a14d1cc7ab0a5
ViktorSheb <vshebela@gmail.com>
2023-04-05 20:08:50 -0400

    Just made my branch and Class

---
+++ src/test/java/ViktoriiaShebelaTest.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ViktoriiaShebelaTest {
+
+
+    @Test
+
+    public void doFirstTest() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+}

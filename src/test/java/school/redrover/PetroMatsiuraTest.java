0499a89bc877891d9ce58df7b1f5b3d140bf5014
Peter <matsstang82@gmail.com>
2023-04-07 17:34:25 -0500

    added store test

---
+++ src/test/java/school/redrover/PetroMatsiuraTest.java
@@ -0,0 +1,22 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PetroMatsiuraTest {
+    @Test
+    public void storeTest() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.google.com/");
+        driver.findElement(By.linkText("Store")).click();
+        Thread.sleep(2000);
+        Assert.assertEquals(driver.getTitle(), "Google Store for Google Made Devices & Accessories");
+        driver.quit();
+    }
+}

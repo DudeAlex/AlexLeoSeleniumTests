aed0d470eb17c52296b2725f4fd78d2e3144bc2d
Peter <matsstang82@gmail.com>
2023-04-08 15:47:28 -0500

    deleted unused import

--- src/test/java/school/redrover/AlexLeoEpicGroupTest.java
+++ src/test/java/school/redrover/AlexLeoEpicGroupTest.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.time.Duration;

08e5c9a5253463c36f54180c10680b459a7bb8b8
Mariia Oia <110502193+MariiaOia@users.noreply.github.com>
2023-04-08 22:50:13 +0300

    add uscistest (#38)
    * add uscistest
    
    * add uscistest
    
    * fix uscis test

--- src/test/java/school/redrover/JavaNiSyTest.java
+++ src/test/java/school/redrover/JavaNiSyTest.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -41,6 +42,31 @@ public class JavaNiSyTest {
         radioBtn.click();
 
         Assert.assertEquals(browser.findElement(By.cssSelector(".mt-3")).getText(), "You have selected Yes");
+        browser.quit();
+    }
+
+    @Test
+    public void testUscis() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver browser = new ChromeDriver(chromeOptions);
+
+        browser.get("https://www.uscis.gov/");
+        WebElement textBox = browser.findElement(By.id("gsc-i-id1"));
+
+        textBox.sendKeys("citizenship and naturalization");
+        textBox.sendKeys(Keys.RETURN);
+
+        Thread.sleep(2000);
+
+        WebElement button = browser.findElement(By.name("Search"));
+        button.click();
+
+        WebElement text = browser.findElement(By.className("gsc-selected-option"));
+
+        Assert.assertEquals(text.getText(), "Relevance");
+
         browser.quit();
     }
 }

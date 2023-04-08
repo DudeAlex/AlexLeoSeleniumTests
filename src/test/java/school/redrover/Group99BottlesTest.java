69fc310f0240fdc63ffc6297709cfba09484ca6b
verafes <verafes@gmail.com>
2023-04-08 09:15:32 -0700

    First test added <verafes>

--- src/test/java/school/redrover/Group99BottlesTest.java
+++ src/test/java/school/redrover/Group99BottlesTest.java
@@ -1,6 +1,9 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -18,6 +21,28 @@ public class Group99BottlesTest {
         driver.get("https://petstore.octoperf.com/actions/Catalog.action");
 
         Assert.assertEquals(driver.getTitle(), "JPetStore Demo");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testCheckboxesHeadHerokuApp() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://formy-project.herokuapp.com/");
+
+
+        driver.findElement(By.xpath("//li/a[@href='/checkbox']")).click();
+
+        Thread.sleep(3000);
+
+        WebElement text = driver.findElement(By.xpath("//h1"));
+
+        Assert.assertEquals(text.getText(), "Checkboxes");
 
         driver.quit();
     }

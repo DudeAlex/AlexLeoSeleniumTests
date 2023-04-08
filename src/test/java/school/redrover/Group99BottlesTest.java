9fbec85433185bf890eb3bfdd61f79f1b08e7c45
AleksandrAkimovChg <104619642+AleksandrAkimovChg@users.noreply.github.com>
2023-04-08 14:08:55 +0300

    added first test (#24)
    * added first test
    
    * fфвÐfix, added headless mode

---
+++ src/test/java/school/redrover/Group99BottlesTest.java
@@ -0,0 +1,25 @@
+package school.redrover;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Group99BottlesTest {
+
+    @Test
+    public void testSecond() {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://petstore.octoperf.com/actions/Catalog.action");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("JPetStore Demo", title);
+
+        driver.quit();
+    }
+}

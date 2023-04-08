7bf4d4ef70c7718cd1e615d6078a494dd4613073
Andrey Bozhko <556070@gmail.com>
2023-04-08 11:21:07 +0300

    Add JavaExpertsGroup tests

---
+++ src/test/java/school/redrover/JavaExpertsGroup.java
@@ -0,0 +1,38 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class JavaExpertsGroup {
+    @Test
+    public void testFirst() throws InterruptedException {
+
+        WebDriver driver = new SafariDriver();
+
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+//        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        Thread.sleep(3000);
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+
+        driver.quit();
+    }
+}

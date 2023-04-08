0cb39d66d109394d6532841196a2b67369b87d23
Anastasia Yakimova <anastasiayy@yahoo.com>
2023-04-08 13:12:22 -0400

    first test  (#34)
    * add testHeaderH1TextOnWomenPage()

--- src/test/java/school/redrover/Group99BottlesTest.java
+++ src/test/java/school/redrover/Group99BottlesTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -35,7 +34,6 @@ public class Group99BottlesTest {
 
         driver.get("https://formy-project.herokuapp.com/");
 
-
         driver.findElement(By.xpath("//li/a[@href='/checkbox']")).click();
 
         Thread.sleep(3000);
@@ -43,6 +41,25 @@ public class Group99BottlesTest {
         WebElement text = driver.findElement(By.xpath("//h1"));
 
         Assert.assertEquals(text.getText(), "Checkboxes");
+
+        driver.quit();
+    }
+
+    @Test
+    public void testHeaderH1TextOnWomenPage() {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://askomdch.com/");
+
+        driver.findElement(By.id("menu-item-1229")).click();
+
+        WebElement headerText = driver.findElement(
+                By.xpath("//header[@class = 'woocommerce-products-header']/h1"));
+
+        Assert.assertEquals(headerText.getText(), "Women");
 
         driver.quit();
     }

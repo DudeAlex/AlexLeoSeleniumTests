ae0248c0bfb7f8defbcaac72ddf4f524efc0268e
Yulia Dmitrieva <yulia.dmitrieva@yahoo.com>
2023-04-08 16:58:46 +0300

    added chromeOptions

--- src/test/java/school/redrover/GroupHighwayToAqaTest.java
+++ src/test/java/school/redrover/GroupHighwayToAqaTest.java
@@ -15,7 +15,7 @@ public class GroupHighwayToAqaTest {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
 
         driver.get("https://magento.softwaretestingboard.com/");
         driver.findElement(By.xpath("//footer/div/ul[@class='footer links']//a[text()='Contact Us']")).click();

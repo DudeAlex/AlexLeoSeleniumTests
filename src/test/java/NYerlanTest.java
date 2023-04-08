cdc3abe4dc1d6d37f9ebaf5c22ff4738a718a7c6
nyerlan <n.yerlan@gmail.com>
2023-04-07 21:10:20 -0700

    refactor add chromeOptions

--- src/test/java/NYerlanTest.java
+++ src/test/java/NYerlanTest.java
@@ -2,36 +2,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class NYerlanTest {
 
     private final String SITE_NAME = "AskOmDch – Become a Selenium automation expert!";
     private final String DISCOUNT_INFO = "25% OFF On all products";
-    private WebDriver driver;
-
-    @BeforeMethod
-    public void beforeMethod() {
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com/");
-    }
-
-    @AfterMethod
-    public void afterMethod() {
-        driver.quit();
-    }
 
 
     @Test
     public void testAssertDiscountInfo_TC_003_01() throws InterruptedException {
-
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://askomdch.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement textDiscount = driver.findElement(By.xpath("//h3[text()='25% OFF On all products']"));
 
         Assert.assertEquals(textDiscount.getText(), DISCOUNT_INFO);

d8869e183575b3cdcc84fb480a1a0921ccca9c47
Yuliya48 <120036999+Yuliya48@users.noreply.github.com>
2023-04-09 10:45:57 -0400

    Second test (#82)
    * add second test

--- src/test/java/school/redrover/GroupJavaQaTeamTest.java
+++ src/test/java/school/redrover/GroupJavaQaTeamTest.java
@@ -46,4 +46,47 @@ public class GroupJavaQaTeamTest {
 
         driver.quit();
     }
+
+    @Test
+    public void testH2TagText_WhenSearchingCityOrlando() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        String cityName = "Orlando";
+        String expectedResult = "Orlando, US";
+
+        driver.get("https://openweathermap.org/ ");
+
+        Thread.sleep(5000);
+
+        WebElement searchCityField = driver.findElement(
+                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']"));
+
+        searchCityField.click();
+        searchCityField.sendKeys(cityName);
+
+        WebElement searchButton = driver.findElement(By.xpath("//button[@type = 'submit']"));
+        searchButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement charlotteDropdownMenu = driver.findElement(
+                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Orlando, US ']"));
+        charlotteDropdownMenu.click();
+
+        WebElement h2CityNameHeader = driver.findElement(
+                By.xpath("//div[@class = 'section-content']/div/div/div/h2"));
+
+        Thread.sleep(1000);
+
+        String actualResult = h2CityNameHeader.getText();
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+
+    }
+
 }

eea535bf42686ed407523551140b73f08208b631
kadysheva natalya <kadysheva.natalya@nedra.digital>
2023-04-09 22:57:42 +0300

    Add my firsttest

---
+++ src/test/java/school/redrover/NatalyaKadyshevaFirstTest.java
@@ -0,0 +1,41 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+
+
+import static org.testng.Assert.assertEquals;
+
+public class NatalyaKadyshevaFirstTest {
+    @Test
+    public void eightComponents() throws InterruptedException {
+        /*FirefoxOptions firefoxOptions= new FirefoxOptions(); //для сервера, не для локального
+        firefoxOptions.addArguments("--headless", "--window-size=1920,1080");*/
+
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        assertEquals("Web form", title);
+
+        Thread.sleep(2000);
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        assertEquals("Received!", value);
+
+        driver.quit();
+    }
+
+}
+

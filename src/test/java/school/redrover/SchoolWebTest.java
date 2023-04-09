e6ad68328a646455cadcc2f60e27b8a2d685072f
Oleg <57632851+eratheo@users.noreply.github.com>
2023-04-09 12:15:59 -0500

    Added Thread.sleep

--- src/test/java/school/redrover/SchoolWebTest.java
+++ src/test/java/school/redrover/SchoolWebTest.java
@@ -5,14 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class SchoolWebTest {
     String[] links = {"Courses", "Training", "About school", "Teachers"};
-    @Ignore
+
     @Test
-    public void testCopyright() {
+    public void testCopyright() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
@@ -21,6 +20,7 @@ public class SchoolWebTest {
 
         for (String link : links) {
             driver.findElement(new By.ByLinkText(link)).click();
+            Thread.sleep(3000);
             String copyright = driver.findElement(By.xpath("//*[contains(text(), 'Copyright ©')]")).getText();
             Assert.assertEquals(copyright, "Copyright © 2022 RedRover School. All rights reserved");
         }

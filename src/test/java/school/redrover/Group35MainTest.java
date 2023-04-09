196eb1632e1b45c4eb481d1b0f66f45f18ffa489
VVaryukhin <slavavar@gmail.com>
2023-04-09 18:21:35 +0300

    init Group35MainTest

---
+++ src/test/java/school/redrover/Group35MainTest.java
@@ -0,0 +1,12 @@
+package school.redrover;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Group35MainTest {
+
+    @Test
+    public void simpleTest_01() {
+        Assert.assertEquals(2, 2);
+    }
+}

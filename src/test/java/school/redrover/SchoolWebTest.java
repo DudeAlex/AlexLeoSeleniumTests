562785ef0d7bd4516de471478f4548dc8447635e
Oleg <57632851+eratheo@users.noreply.github.com>
2023-04-08 23:39:51 -0500

    Oy/test school web (#52)
    * Added test to check year in copyright. Only Teachers page now.
    
    * Added test to check copyright.
    
    * Removed unnecessary class.
    Moved everything to single test.
    
    * Added quit(). Updated for loop.

---
+++ src/test/java/school/redrover/SchoolWebTest.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SchoolWebTest {
+    String[] links = {"Courses", "Training", "About school", "Teachers"};
+    @Test
+    public void testCopyright() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://redrover.school");
+
+        for (String link : links) {
+            driver.findElement(new By.ByLinkText(link)).click();
+            String copyright = driver.findElement(By.xpath("//*[contains(text(), 'Copyright ©')]")).getText();
+            Assert.assertEquals(copyright, "Copyright © 2022 RedRover School. All rights reserved");
+        }
+
+        driver.quit();
+    }
+}

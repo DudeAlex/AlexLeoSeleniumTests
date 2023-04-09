9cb2749230f68031460d2874d8df19c206bd0ae0
MariaShy <68644507+MariaShy@users.noreply.github.com>
2023-04-09 11:54:38 +0300

    add GroupMaxTest with first test (#65)
    * add GroupMaxTest with first test
    
    * fix imports
    
    * ignore SvetlanaTest
    
    * ignore testCheckTheChartIsEmpty
    
    ---------
    
    Co-authored-by: Maria Shy <24011989@mail.ru>

---
+++ src/test/java/school/redrover/GroupMaxTest.java
@@ -0,0 +1,23 @@
+package school.redrover;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GroupMaxTest {
+
+    @Test
+    public void testSaucedemoTitle(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.saucedemo.com/");
+
+        Assert.assertEquals(driver.getTitle(), "Swag Labs");
+
+        driver.quit();
+    }
+}

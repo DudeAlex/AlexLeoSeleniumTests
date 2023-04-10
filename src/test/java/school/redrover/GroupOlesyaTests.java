a416dce3f562279de2ceca47cdd13472157d50dd
Stoyana <101792053+StoyanaDi@users.noreply.github.com>
2023-04-10 06:56:37 +0300

    adding my first tests to GitHub (#96)
    Add first test to github

--- src/test/java/school/redrover/GroupOlesyaTests.java
+++ src/test/java/school/redrover/GroupOlesyaTests.java
@@ -5,8 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class GroupOlesyaTests {
     @Test
@@ -43,5 +48,92 @@ public class GroupOlesyaTests {
          Assert.assertEquals(driver.findElements(By.xpath("//*[@id=\"remove-sauce-labs-backpack\"]")).isEmpty(), true);
 
          driver.quit();
+    }
+
+    @Test
+    public void checkPriceLowToHigh() { //Stoyana's Test
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement login = driver.findElement(By.name("user-name"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement button = driver.findElement(By.name("login-button"));
+
+        login.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        button.click();
+
+        List<WebElement> beforeFilterPrice = driver.findElements(By.className("inventory_item_price"));
+        List<Double> beforeFilterPriceList = new ArrayList<>();
+
+        for (WebElement e : beforeFilterPrice) {
+            beforeFilterPriceList.add(Double.valueOf(e.getText().replace("$", "")));
+        }
+
+        WebElement funnelIcon = driver.findElement(By.className("select_container"));
+        funnelIcon.click();
+
+        Select drpOrder = new Select(driver.findElement(By.className("product_sort_container")));
+        drpOrder.selectByVisibleText("Price (low to high)");
+
+        List<WebElement> afterFilterPrice = driver.findElements(By.className("inventory_item_price"));
+        List<Double> afterFilterPriceList = new ArrayList<>();
+
+        for (WebElement e : afterFilterPrice) {
+            afterFilterPriceList.add(Double.valueOf(e.getText().replace("$", "")));
+        }
+        Collections.sort(beforeFilterPriceList);
+
+        Assert.assertEquals(beforeFilterPriceList, afterFilterPriceList);
+
+        driver.quit();
+    }
+
+    @Test
+    public void checkPriceHighToLow() { //Stoyana's Test
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement login = driver.findElement(By.name("user-name"));
+        WebElement password = driver.findElement(By.name("password"));
+        WebElement button = driver.findElement(By.name("login-button"));
+
+        login.sendKeys("standard_user");
+        password.sendKeys("secret_sauce");
+        button.click();
+
+        List<WebElement> beforeFilterPrice = driver.findElements(By.className("inventory_item_price"));
+        List<Double> beforeFilterPriceList = new ArrayList<>();
+
+        for (WebElement e : beforeFilterPrice) {
+            beforeFilterPriceList.add(Double.valueOf(e.getText().replace("$", "")));
+        }
+
+        WebElement funnelIcon = driver.findElement(By.className("select_container"));
+        funnelIcon.click();
+
+        Select drpOrder = new Select(driver.findElement(By.className("product_sort_container")));
+        drpOrder.selectByVisibleText("Price (high to low)");
+
+        List<WebElement> afterFilterPrice = driver.findElements(By.className("inventory_item_price"));
+        List<Double> afterFilterPriceList = new ArrayList<>();
+
+        for (WebElement e : afterFilterPrice) {
+            afterFilterPriceList.add(Double.valueOf(e.getText().replace("$", "")));
+        }
+        Collections.sort(beforeFilterPriceList);
+        Collections.reverse(beforeFilterPriceList); //reverse the sorted list
+
+        Assert.assertEquals(beforeFilterPriceList, afterFilterPriceList);
+
+        driver.quit();
     }
 }

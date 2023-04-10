e50e21a081787fb02965ff369f1eda4b3651965f
Tatsiannna <tatsiannap@gmail.com>
2023-04-10 04:02:30 -0400

    FIXED Test_TC_001 first commit Test_TC_002 first commit

--- src/test/java/TatsiPkTest.java
+++ src/test/java/TatsiPkTest.java
@@ -1,10 +1,15 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
+import org.testng.annotations.Test;
 
 public class TatsiPkTest {
 
+    @Ignore
+    @Test
     public void Test_TC_001_Original() throws InterruptedException {
 
         WebDriver driver = new ChromeDriver();
@@ -20,6 +25,21 @@ public class TatsiPkTest {
 
         String saleTag = driver.findElement(By.xpath("//span[@class=\"onsale\"]")).getText();
         Assert.assertEquals(saleTag, "Sale!");
+
+        driver.quit();
+    }
+
+    @Test
+    public void Test_TC_003_Original() throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://askomdch.com/");
+
+        Thread.sleep(2000);
+
+        WebElement text = driver.findElement(By.xpath("//h3[contains(text(), '25%')]"));
+        Assert.assertEquals(text.getText(), "25% OFF On all products");
 
         driver.quit();
     }

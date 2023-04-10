fe570189d459ce4919f7c52d03927e4608568258
Uladzimir Tarasavets <77638965+VladimirTarasovets@users.noreply.github.com>
2023-04-10 19:05:48 +0300

    add steam test (#152)

--- src/test/java/school/redrover/GroupILoveBugsTest.java
+++ src/test/java/school/redrover/GroupILoveBugsTest.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class GroupILoveBugsTest {
     @Test
@@ -24,6 +28,30 @@ public class GroupILoveBugsTest {
         WebElement latestNews = driver.findElement(By.xpath("//h2[@class='posts-block-title']"));
 
         Assert.assertEquals(latestNews.getText(), "LATEST NEWS");
+
+        driver.quit();
+    }
+
+    @Test
+    public void SteamTest(){
+        final String MAIN_PAGE = "https://store.steampowered.com/";
+        final By LOGIN_BUTTON = By.xpath("//a[@class='global_action_link']");
+        final By SIGN_IN_BUTTON = By.cssSelector(".newlogindialog_SubmitButton_2QgFE");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(4));
+
+        driver.get(MAIN_PAGE);
+        Assert.assertEquals(driver.getCurrentUrl(), MAIN_PAGE, "The page opened is not 'store.steampowered.com' or the URL is invalid");
+
+        wait.until(ExpectedConditions.elementToBeClickable(LOGIN_BUTTON));
+        driver.findElement(LOGIN_BUTTON).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(SIGN_IN_BUTTON));
+        Assert.assertTrue(driver.findElement(SIGN_IN_BUTTON).isDisplayed(), "The bottom 'Sign In' is not displayed.");
 
         driver.quit();
     }

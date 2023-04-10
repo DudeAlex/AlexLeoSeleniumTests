e50944a36d64dadf7226e3ac6445631a65adc33e
RomanIvanov-atom <107417681+RomanIvanov-atom@users.noreply.github.com>
2023-04-10 22:48:56 +0300

    First merge for javaExplorers group into main branch (#168)
    * First commit for GroupClass
    
    * First commit for GroupClass

--- src/test/java/school/redrover/GroupJavaExplorersTest.java
+++ src/test/java/school/redrover/GroupJavaExplorersTest.java
@@ -1,6 +1,8 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -20,6 +22,44 @@ public class GroupJavaExplorersTest {
         driver.get(url);
 
         Assert.assertEquals(driver.getTitle(), expTitle);
+
+        driver.quit();
+    }
+
+    @Test
+    public void vhodTextOnAuthPageTest() throws InterruptedException {
+
+        //проверка того, что на окошке со входом над полями Логина и пасса есть слово Вход
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        //"--remote-allow-origins=*", "--headless" --- params for server
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.21vek.by/");             //проходим на сайт
+
+        Thread.sleep(1000);
+
+        WebElement buttonCookie = driver.findElement(    //прошли выплывашку с куки
+                By.xpath("//button[@class='Button-module__button Button-module__blue-primary']"));
+        buttonCookie.click();
+
+        Thread.sleep(1000);
+
+        WebElement buttonAccount = driver.findElement(   // нажали на Аккаунт
+                By.xpath("//button[@class='styles_userToolsToggler__imcSl']"));
+        buttonAccount.click();
+
+        Thread.sleep(1000);
+
+        WebElement buttonVoiti = driver.findElement(   // нажали на Войти
+                By.xpath("//button[@data-testid='loginButton']"));
+        buttonVoiti.click();
+
+        Thread.sleep(1000);
+
+        WebElement textVhod = driver.findElement(   // нашли текст вверху, в центре, где должно быть ВХОД
+                By.xpath("//h5[@class='style_formTitle__hRNRz']"));
+        Assert.assertEquals(textVhod.getText(), "Вход");
 
         driver.quit();
     }

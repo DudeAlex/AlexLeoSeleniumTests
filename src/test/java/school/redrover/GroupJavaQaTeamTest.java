b8cbd5052873b2160ac65f51527f6dc4e8cda6e3
KateHalavina <k.halavina13@gmail.com>
2023-04-11 12:58:52 -0400

    My first test/ changes

--- src/test/java/school/redrover/GroupJavaQaTeamTest.java
+++ src/test/java/school/redrover/GroupJavaQaTeamTest.java
@@ -113,7 +113,7 @@ public class GroupJavaQaTeamTest {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
 
         String title = driver.getTitle();

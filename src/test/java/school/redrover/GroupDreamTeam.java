d86f7c97cd46bae6abfcbf771242b6b640b2452c
Natalia <nsirtak@gmail.com>
2023-04-11 18:43:57 -0500

    changes added by natalia

--- src/test/java/school/redrover/GroupDreamTeam.java
+++ src/test/java/school/redrover/GroupDreamTeam.java
@@ -53,4 +53,30 @@ public class GroupDreamTeam {
 
         driver.quit();
     }
+    @Test
+    public void testSecond() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("Web form", title);
+
+        Thread.sleep(2000);
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+
+        textBox.sendKeys("Selenium");
+        submitButton.click();
+
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        Assert.assertEquals("Received!", value);
+
+        driver.quit();
+    }
 }

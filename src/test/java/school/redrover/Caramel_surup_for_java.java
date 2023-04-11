75f8f0d32d70dab64fa0b25399e1349076e0b11d
ArtyomDulya <111312308+ArtyomDulya@users.noreply.github.com>
2023-04-11 09:18:09 +0500

    commit (#199)
    * commit
    
    * https://thehostbest.ru/
    
    * SearchLineHeader

--- src/test/java/school/redrover/Caramel_surup_for_java.java
+++ src/test/java/school/redrover/Caramel_surup_for_java.java
@@ -1,31 +1,107 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.ArrayList;
 
 public class Caramel_surup_for_java {
 
     @Test
-    public void artyomDulyaOpenWeatherGuideClick () throws InterruptedException {
+    public void artyomDulyaOpenWeatherGuideClick() throws InterruptedException {
+
+        String expectedResultUrl = "https://openweather.co.uk/";
+        String expectedResultBanner = "Complete spectrum of weather data solutions.";
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
 
         driver.get("https://openweathermap.org/");
         Thread.sleep(5000);
-        WebElement orderSite = driver.findElement(By.xpath("//ul//div//ul/li//a[@href='/guide']"));
-        orderSite.click();
-        WebElement turnkeyBusinessCard = driver.findElement(
+        WebElement guide = driver.findElement(By.xpath("//ul//div//ul/li//a[@href='/guide']"));
+
+        guide.click();
+
+        WebElement complexEnterprise = driver.findElement(
                 By.xpath("//main//div[2]/div/div/p[1]/a[text()='complex enterprise systems']"));
-        turnkeyBusinessCard.click();
+        complexEnterprise.click();
+
+        ArrayList<String> windows = new ArrayList<String>(driver.getWindowHandles());
+        driver.switchTo().window(windows.get(1));
+
+        WebElement text = driver.findElement(By.xpath("//h1//span[@class='white-text']"));
+
+        String actualResultUrl = driver.getCurrentUrl();
+        String actualResultBanner = text.getText();
+
+        Assert.assertEquals(actualResultUrl, expectedResultUrl);
+        Assert.assertEquals(actualResultBanner, expectedResultBanner);
 
         driver.quit();
+    }
+
+    @Test
+    public void artyomDulyaThehostbest() throws InterruptedException {
+
+        String expectedResult = "https://thehostbest.ru/business-card-site/";
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.get("https://thehostbest.ru/");
+
+        WebElement button = driver.findElement(By.xpath("//div[@data-id='ee672f3']//div//div//a"));
+        js.executeScript("window.scrollBy(0,200)");
+        button.click();
+
+        String actualResult = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+
+    @Test
+    public void artyomDulyaSearchLineHeader() throws InterruptedException {
+
+        String expectedResult = "Paris, FR";
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+
+        driver.get("https://openweathermap.org/");
 
 
+        String selectorSearchLine = "//ul[@id='first-level-nav']//div//form//input[@placeholder='Weather in your city']";
+        WebElement searchLineHeader = driver.findElement(By.xpath(selectorSearchLine));
+        searchLineHeader.sendKeys("Paris\n");
+
+        WebElement paris = driver.findElement(By.xpath("//td//b//a[@href='/city/2988507']"));
+        paris.click();
+
+
+        WebElement parisText = driver.findElement(By.tagName("h2"));
+
+        String actualResult = parisText.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
     }
 }

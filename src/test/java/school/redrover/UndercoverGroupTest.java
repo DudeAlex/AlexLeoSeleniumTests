aa8a5cfe376333d503007f861af0b31ecfe119e7
patchworkdude <125486425+patchworkdude@users.noreply.github.com>
2023-04-11 23:10:02 +0200

    add dragAndDropTest (#247)
    * add dragAndDropTest

--- src/test/java/school/redrover/UndercoverGroupTest.java
+++ src/test/java/school/redrover/UndercoverGroupTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -61,6 +62,30 @@ public class UndercoverGroupTest {
             System.out.println("First checkbox checked!");
         } else {
             System.out.println("Checkbox not found");
+        }
+    }
+
+    @Test
+    public void dragAndDropTest() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
+            driver.get("https://crossbrowsertesting.github.io/drag-and-drop.html");
+            Thread.sleep(2000);
+
+            WebElement element1 = driver.findElement(By.id("draggable"));
+            WebElement element2 = driver.findElement(By.id("droppable"));
+
+            Actions action = new Actions(driver);
+
+            action.dragAndDrop(element1, element2).build().perform();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            Thread.sleep(2000);
+            driver.quit();
         }
     }
 }

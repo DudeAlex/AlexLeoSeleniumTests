42f5f848526c59b8cbe04bf95f376e29b562d171
OlegFedUS <104395182+OlegFedUS@users.noreply.github.com>
2023-04-11 04:37:07 -0400

    OlegfTest first test. (#213)
    * OlegfTest first test.
    
    * OlegfTest FIX.
    
    ---------
    
    Co-authored-by: FedorovOlegUS <FedorovOlUS@gmail.com>

---
+++ src/test/java/school/redrover/OlegfTest.java
@@ -0,0 +1,35 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class OlegfTest {
+
+    @Test
+    public void dishesTest() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        driver.get("https://www.canvashomestore.com/");
+
+        driver.findElement(By.name("q")).sendKeys("plate\n");
+        List<WebElement> platesList = driver.findElements(By.xpath("//main//div[@class='grid__item']"));
+
+        String finalPlate;
+        for (int i = 0; i < platesList.size(); i++) {
+            finalPlate = platesList.get(i).getText();
+            assert finalPlate.contains("Blue");
+        }
+        driver.quit();
+    }
+}

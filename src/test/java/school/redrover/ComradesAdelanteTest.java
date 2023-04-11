1784b2a6b0fbf6a645e4501777bb3a9058635c3c
MariaLuchnikova <124650461+MariaLuchnikova@users.noreply.github.com>
2023-04-11 14:33:34 +0800

    Add ML commit (#95)
    * Add ML commit
    ---------
    Co-authored-by: Nikolay Zanin <40330794+NickZanin@users.noreply.github.com>

--- src/test/java/school/redrover/ComradesAdelanteTest.java
+++ src/test/java/school/redrover/ComradesAdelanteTest.java
@@ -10,6 +10,8 @@ import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ComradesAdelanteTest {
 
     @Ignore
@@ -100,5 +102,45 @@ public class ComradesAdelanteTest {
         Assert.assertEquals(actualResult, expectedResult);
 
         driver.quit();
+    }
+
+    @Test
+    public void MariaLuchnikovaTest() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.saucedemo.com");
+
+        WebElement login = driver.findElement(By.id("user-name"));
+        login.sendKeys("standard_user");
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys("secret_sauce");
+
+        WebElement loginButton = driver.findElement(By.id("login-button"));
+        loginButton.click();
+
+        //Thread.sleep(3000);
+
+        WebElement Backpack = driver.findElement(By.id("add-to-cart-sauce-labs-backpack"));
+        Backpack.click();
+
+        WebElement tShirt = driver.findElement(By.id("add-to-cart-sauce-labs-bolt-t-shirt"));
+        tShirt.click();
+
+        WebElement onesie = driver.findElement(By.id("add-to-cart-sauce-labs-onesie"));
+        onesie.click();
+
+        WebElement cart = driver.findElement(By.className("shopping_cart_link"));
+        cart.click();
+
+        List<WebElement> items = driver.findElements(By.className("cart_item"));
+
+        int countItems = items.size();
+        Assert.assertEquals(countItems, 3);
+
+        driver.quit();
+
     }
 }

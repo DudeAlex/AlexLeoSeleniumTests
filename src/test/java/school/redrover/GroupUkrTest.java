0c9dc32cead1503d732df65e4e1e27efc4bd14b4
Bohdan Sydorenko <at3c86@gmail.com>
2023-04-11 21:40:14 +0300

    Test comment (#248)
    test commit

---
+++ src/test/java/school/redrover/GroupUkrTest.java
@@ -0,0 +1,11 @@
+package school.redrover;
+
+import org.testng.annotations.Test;
+
+public class GroupUkrTest {
+
+    @Test
+    public void HelloWorldTest(){
+        System.out.println("Hello World");
+    }
+}

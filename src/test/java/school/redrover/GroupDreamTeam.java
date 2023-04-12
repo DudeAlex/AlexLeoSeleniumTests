c3c32a7d021a84f4dc54d9cf6ceb625b2174ef94
Liliana Mita <lmita0708@gmail.com>
2023-04-12 18:20:49 -0500

    Lm/mysec test (#365)
    * some comment
    
    * some comment
    
    * committing
    
    * committing
    
    * added comment
    
    * added comment
    
    * added comment
    
    * message
    
    * Add my test
    
    * Add my test with changes
    
    ---------
    
    Co-authored-by: lilianamita <imita0708@gmail.com>

--- src/test/java/school/redrover/GroupDreamTeam.java
+++ src/test/java/school/redrover/GroupDreamTeam.java
@@ -48,6 +48,7 @@ public class GroupDreamTeam {
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get(url);
 
+
         String actualResult = driver.getCurrentUrl();
         Assert.assertEquals(actualResult, expectedResult);
 
@@ -55,6 +56,9 @@ public class GroupDreamTeam {
     }
     @Test
     public void testSecond() throws InterruptedException {
+
+
+        //comment
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
@@ -73,10 +77,50 @@ public class GroupDreamTeam {
         textBox.sendKeys("Selenium");
         submitButton.click();
 
+        WebElement password = driver.findElement(By.name("my-password"));
+        Thread.sleep(2000);
+        textBox.sendKeys("Selenium");
+
+        password.sendKeys("WebSel1");
+        Thread.sleep(2000);
+        submitButton.click();
+        Thread.sleep(2000);
         WebElement message = driver.findElement(By.id("message"));
         String value = message.getText();
         Assert.assertEquals("Received!", value);
 
         driver.quit();
     }
+    @Test
+    public void testSecondLilia() throws InterruptedException { 
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("Web form", title);
+
+        Thread.sleep(2000);
+
+        WebElement textBox = driver.findElement(By.name("my-text"));
+        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement password = driver.findElement(By.name("my-password"));
+        WebElement textarea = driver.findElement(By.xpath("//textarea"));
+        Thread.sleep(2000);
+        textBox.sendKeys("Selenium");
+        password.sendKeys("WebSel1");
+        textarea.sendKeys("I am working with Selenium");
+        Thread.sleep(2000);
+        submitButton.click();
+        Thread.sleep(2000);
+        WebElement message = driver.findElement(By.id("message"));
+        String value = message.getText();
+        Assert.assertEquals("Received!", value);
+
+        driver.quit();
+    }
+
 }

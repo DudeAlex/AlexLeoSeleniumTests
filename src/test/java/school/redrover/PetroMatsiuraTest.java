a03ae69790b2b076fd93777407b721dbc64bcaba
Peter <matsstang82@gmail.com>
2023-04-12 01:49:27 -0500

    added googleAppsTest

--- src/test/java/school/redrover/PetroMatsiuraTest.java
+++ src/test/java/school/redrover/PetroMatsiuraTest.java
@@ -7,18 +7,39 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class PetroMatsiuraTest {
 
     @Test
-    public void storeTest() throws InterruptedException {
+    public void googleStoreTest() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
         WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.google.com/");
-        Thread.sleep(5000);
         driver.findElement(By.linkText("Store")).click();
-        Thread.sleep(5000);
+        driver.navigate().refresh();
         Assert.assertEquals(driver.getTitle(), "Google Store for Google Made Devices & Accessories");
+        driver.quit();
+    }
+
+
+    @Test
+    public void googleAppsTest() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://www.google.com/");
+        for (int i = 1; i < 4; i++) {
+            String [] result = {"Google Account", "Google", "Google Maps"};
+            driver.navigate().refresh();
+            driver.findElement(By.cssSelector("a[aria-label='Google apps']")).click();
+            driver.switchTo().frame("app").findElement(By.xpath("//div[1]/ul/li[" + i + "]")).click();
+            Assert.assertEquals(driver.getTitle(), "" + result[i - 1] + "");
+            driver.navigate().back();
+        }
         driver.quit();
     }
 }

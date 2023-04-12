1d649adc9a8716319c799e9efb456737300061af
ArtyomDulya <111312308+ArtyomDulya@users.noreply.github.com>
2023-04-13 00:34:31 +0500

    remoute (#349)

--- src/test/java/school/redrover/CaramelSurupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSurupForJavaTest.java
@@ -11,24 +11,8 @@ import org.testng.annotations.Test;
 
 public class CaramelSurupForJavaTest {
 
-    @Ignore
-    @Test
-    public void artyomDulyaOpenWeatherGuideClick () throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(chromeOptions);
 
-        driver.get("https://openweathermap.org/");
-        Thread.sleep(5000);
-        WebElement orderSite = driver.findElement(By.xpath("//ul//div//ul/li//a[@href='/guide']"));
-        orderSite.click();
-        WebElement turnkeyBusinessCard = driver.findElement(
-                By.xpath("//main//div[2]/div/div/p[1]/a[text()='complex enterprise systems']"));
-        turnkeyBusinessCard.click();
-
-        driver.quit();
-    }
 
     @Test
     public void serhiiHaponiukFirstSeleniumTest() throws InterruptedException {

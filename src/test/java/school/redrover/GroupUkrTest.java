189e985d402ee808515f43ea39796e8ae458d7c9
Bohdan Sydorenko <at3c86@gmail.com>
2023-04-13 01:31:58 +0300

    test searching the link to video by title (#363)
    * test searching the link to video by title
    
    * delete comments, update waiters

--- src/test/java/school/redrover/GroupUkrTest.java
+++ src/test/java/school/redrover/GroupUkrTest.java
@@ -1,11 +1,45 @@
 package school.redrover;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class GroupUkrTest {
 
     @Test
-    public void HelloWorldTest(){
-        System.out.println("Hello World");
+    public void youtubeSearchTest(){
+        ChromeOptions optionsChrome = new ChromeOptions();
+        optionsChrome.addArguments("--headless","--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(optionsChrome);
+        driver.get("https://www.youtube.com/");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("YouTube", title);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+
+        WebElement searchInput = driver.findElement(By.xpath("//input[@id='search']"));
+        WebElement searchButton = driver.findElement(By.xpath("//button[@id='search-icon-legacy']"));
+
+        searchInput.sendKeys("Что такое Selenium?");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+
+        searchButton.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
+
+        WebElement link = driver.findElement(By.xpath("//a[@title='Что такое Selenium?']"));
+        String value = link.getText();
+
+        Assert.assertEquals(value, "Что такое Selenium?");
+
+        driver.quit();
+
     }
 }

3aae150e0ec7fa5b39b726325bc2010d10042307
dashaTu <93719906+dashaTu@users.noreply.github.com>
2023-04-12 17:34:35 +0300

    add testAboutDoctorsFind-2 (#300)

--- src/test/java/school/redrover/GroupJavaJitsu.java
+++ src/test/java/school/redrover/GroupJavaJitsu.java
@@ -173,7 +173,7 @@ public class GroupJavaJitsu {
 
 
     @Test
-    public void testAboutDoctorsFind() {
+    public void testAboutDoctorsFind() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
@@ -181,16 +181,24 @@ public class GroupJavaJitsu {
         driver.get("https://prodoctorov.ru/");
         String title = driver.getTitle();
         Assert.assertEquals("ПроДокторов – сайт отзывов пациентов о врачах №1 в России", title);
+        WebElement urlElement = driver.findElement(By.className("b-choose-town-btn-v2"));
+        urlElement.click();
+        WebElement inputElementSearch = driver.findElement(By.className("b-choose-town-popup__search-input"));
+        inputElementSearch.sendKeys("Краснодар");
+        WebElement SearchboxElement = driver.findElement(By.className("tt-dataset"));
+        Thread.sleep(2000);
+        SearchboxElement.click();
+        Thread.sleep(2000);
         WebElement inputElement = driver.findElement(By.className("text-field__input"));
-        WebElement submitButton = driver.findElement(By.className("base-search__button"));
+        Thread.sleep(2000);
         inputElement.sendKeys("Ницакова Марина Петровна");
+        WebElement submitButton = driver.findElement(By.className("base-search__button"));
         submitButton.click();
         WebElement link = driver.findElement(By.className("b-card__name-link"));
         link.click();
         WebElement text = driver.findElement(By.className("ui-text"));
         Assert.assertEquals(driver.getCurrentUrl(), "https://prodoctorov.ru/krasnodar/vrach/177664-nicakova/");
         driver.quit();
-
     }
     @Test
     public void testAlex() throws InterruptedException {

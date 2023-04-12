57482adc0ee353d7c89e5f27e94a8dfde0a3e6c1
Artemis <56003726+Temartem7@users.noreply.github.com>
2023-04-12 17:28:55 -0400

    Java jitsu tema test (#215)
    * Tema Test
    
    * Tema Test 2
    
    * Tema Test 3

--- src/test/java/school/redrover/GroupJavaJitsu.java
+++ src/test/java/school/redrover/GroupJavaJitsu.java
@@ -1,8 +1,14 @@
 package school.redrover;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -16,7 +22,7 @@ import java.util.List;
 public class GroupJavaJitsu {
 
     @Test
-    public void testCarServiceOptions() {
+    public void testCarServiceOptions(){
 
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
@@ -52,16 +58,84 @@ public class GroupJavaJitsu {
     }
 
     @Test
-    public void tema_openCartRegistrationTest() throws InterruptedException {
+    public void tema_flightFinderTest() throws Throwable {
+//        WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        driver.get("https://demo.opencart.com/index.php?route=account/register&language=en-gb");
-        driver.findElement(By.id("input-firstname")).sendKeys("Tema");
-        driver.findElement(By.id("input-lastname")).sendKeys("Shvets");
-        driver.findElement(By.id("input-email")).sendKeys("temaemail123@gmail.com");
-        driver.findElement(By.id("input-password")).sendKeys("123xyz");
+        String url = "https://demo.guru99.com/test/newtours/reservation.php";
+        driver.get(url);
+
+        WebElement radioBtn = driver.findElement(By.xpath("//input[@value='oneway']"));
+        radioBtn.click();
+
+        //Passenger Selection
+        WebElement passengerAmount = driver.findElement(By.name("passCount"));
+        Select pAmount = new Select(passengerAmount);
+        pAmount.selectByIndex(2);           //3 passengers selected
+        Thread.sleep(2000);
+        pAmount.selectByVisibleText("2");   //changed my mind, selected 2 passengers
+        Thread.sleep(2000);
+        pAmount.selectByValue("4");         //changed my mind, selected 4 passengers
+        Thread.sleep(2000);
+
+        //Departing Airport Selection
+        WebElement dAirport = driver.findElement(By.name("fromPort"));
+        Select dFrom = new Select(dAirport);
+        List<WebElement> dAirports = dFrom.getOptions();
+        for(WebElement ele : dAirports){
+            System.out.println("Departing city: "+ele.getText());
+        }
+        Thread.sleep(2000);
+        dFrom.selectByValue("Paris");
+
+        //Departing Month Selection
+        WebElement dMonths = driver.findElement(By.name("fromMonth"));
+        Select dMonth = new Select(dMonths);
+        Thread.sleep(2000);
+        dMonth.selectByVisibleText("April");
+
+        //Departing Day Selection
+        WebElement dDays = driver.findElement(By.name("fromDay"));
+        Select dDay = new Select(dDays);
+        Thread.sleep(2000);
+        dDay.selectByVisibleText("5");
+
+        //Arriving Airport Selection
+        WebElement aAirport = driver.findElement(By.name("toPort"));
+        Select aTo = new Select(aAirport);
+        Thread.sleep(2000);
+        aTo.selectByValue("Zurich");
+
+        //Arriving Month Selection
+        WebElement aMonths = driver.findElement(By.name("toMonth"));
+        Select aMonth = new Select(aMonths);
+        Thread.sleep(2000);
+        aMonth.selectByVisibleText("May");
+
+        //Arriving Day Selection
+        WebElement aDays = driver.findElement(By.name("toDay"));
+        Select aDay = new Select(aDays);
+        Thread.sleep(2000);
+        aDay.selectByVisibleText("14");
+
+        //Service Class
+        WebElement sClass = driver.findElement(By.xpath("//input[@value='Business']"));
+        sClass.click();
+
+        //Airline
+        WebElement airline = driver.findElement(By.name("airline"));
+        Select selectAirline = new Select(airline);
+        Thread.sleep(2000);
+        airline.click();
+        Thread.sleep(2000);
+
+//        selectAirline.selectByIndex(3);
+        selectAirline.selectByVisibleText("Unified Airlines");
+
+        //checkout
+        driver.findElement(By.name("findFlights")).click();
+        Assert.assertEquals(driver.getCurrentUrl(),"https://demo.guru99.com/test/newtours/reservation2.php");
         driver.quit();
     }
 
@@ -248,7 +322,7 @@ public class GroupJavaJitsu {
 
 
     }
-    
+
     @Ignore
     @Test
     void testFirstMariana() throws InterruptedException {
@@ -312,4 +386,3 @@ public class GroupJavaJitsu {
         driver.quit();
     }
 }
-

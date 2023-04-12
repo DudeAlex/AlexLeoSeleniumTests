ab9208c03c9a043133939eaebcbfbdf2f6898832
SerhiiHaponiuk <gaponyuk1994@gmail.com>
2023-04-12 08:54:18 +0300

    Update CaramelSurupForJavaTest.java

--- src/test/java/school/redrover/CaramelSurupForJavaTest.java
+++ src/test/java/school/redrover/CaramelSurupForJavaTest.java
@@ -6,8 +6,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
-public class Caramel_surup_for_java {
+public class CaramelSurupForJavaTest {
 
     @Test
     public void artyomDulyaOpenWeatherGuideClick () throws InterruptedException {
@@ -25,7 +26,28 @@ public class Caramel_surup_for_java {
         turnkeyBusinessCard.click();
 
         driver.quit();
+    }
 
+    @Test
+    public void serhiiHaponyukFirstSeleniumTest() throws InterruptedException {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.selenium.dev/");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Selenium");
+
+        Thread.sleep(5000);
+//        WebElement accreditation = driver.findElement(By.xpath(
+//                "//u[text()='аккредитованных ИТ-компаний']"));
+//        accreditation.click();
+//        Thread.sleep(5000);
+//        WebElement pageITAccreditationOpened = driver.findElement(By.xpath(
+//                "//div[@class = 't469__title t-title t-title_lg ']"));
+
+//        driver.quit();
 
     }
 }

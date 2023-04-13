d56e8165d4b2fa8c150f46555d00a4296144a074
Valery Nikolaieva <shamanmantik0r@gmail.com>
2023-04-13 09:32:16 +0300

    add new test Valeriia_Nikolaieva

--- src/test/java/school/redrover/GroupUkrTest.java
+++ src/test/java/school/redrover/GroupUkrTest.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -42,4 +44,30 @@ public class GroupUkrTest {
         driver.quit();
 
     }
+    @Test
+    public void VNZooleaderTest(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless","--window-size= 1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://zooleader.com.ua/");
+
+        WebElement srchField = driver.findElement(By.xpath("//header[@class = 'header header--fixed']//input[@id='search']"));
+        WebElement srchBtn = driver.findElement(By.xpath("//header[@class = 'header header--fixed']//button[@class='block-search__btn']"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(srchField));
+
+        srchField.sendKeys("36494");
+        srchBtn.click();
+
+        WebElement resultName = driver.findElement(By.xpath("//section[@class = 'product__hero']//h1[@class='product__title']"));
+
+        Assert.assertTrue(resultName.getText().contains("Davis «Pink Grapefruit»"));
+
+        driver.quit();
+
+    }
+
 }

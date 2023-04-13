b84615a1ae4c30a5c5b380958fbbcff06b828b4c
Valeriia_Nikolaieva <shamanmantik0r@gmail.com>
2023-04-13 10:55:13 +0300

    add new test Valeriia_Nikolaieva

--- src/test/java/school/redrover/GroupUkrTest.java
+++ src/test/java/school/redrover/GroupUkrTest.java
@@ -50,23 +50,26 @@ public class GroupUkrTest {
         chromeOptions.addArguments("--headless","--window-size= 1920,1080");
 
         WebDriver driver = new ChromeDriver(chromeOptions);
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
         driver.get("https://zooleader.com.ua/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(10000));
 
         WebElement srchField = driver.findElement(By.xpath("//header[@class = 'header header--fixed']//input[@id='search']"));
         WebElement srchBtn = driver.findElement(By.xpath("//header[@class = 'header header--fixed']//button[@class='block-search__btn']"));
+        WebElement popupBtn = driver.findElement(By.xpath("//div[@id='onloadModal']//button[@class = 'modal-close close']"));
+            popupBtn.click();
+            wait.until(ExpectedConditions.elementToBeClickable(srchBtn));
 
-        wait.until(ExpectedConditions.elementToBeClickable(srchField));
+            srchField.sendKeys("36494");
+            srchBtn.click();
+            wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//section[@class = 'product__hero']")));
+            WebElement resultName = driver.findElement(By.xpath("//section[@class = 'product__hero']//h1[@class='product__title']"));
 
-        srchField.sendKeys("36494");
-        srchBtn.click();
+            Assert.assertTrue(resultName.getText().contains("Davis «Pink Grapefruit»"));
 
-        WebElement resultName = driver.findElement(By.xpath("//section[@class = 'product__hero']//h1[@class='product__title']"));
+            driver.quit();
 
-        Assert.assertTrue(resultName.getText().contains("Davis «Pink Grapefruit»"));
-
-        driver.quit();
 
     }
 

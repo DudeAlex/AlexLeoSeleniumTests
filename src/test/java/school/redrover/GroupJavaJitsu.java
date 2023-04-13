008aed99c6b769da6b01169aa663ab40cd981f92
Dasha Turevich <dasha.turevich@yandex.ru>
2023-04-13 14:17:29 +0300

    add testAboutDoctorsFind-3

--- src/test/java/school/redrover/GroupJavaJitsu.java
+++ src/test/java/school/redrover/GroupJavaJitsu.java
@@ -180,7 +180,7 @@ public class GroupJavaJitsu {
 
 
     @Test
-    public void testAboutDoctorsFind() {
+    public void testAboutDoctorsFind() throws InterruptedException {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
@@ -188,9 +188,21 @@ public class GroupJavaJitsu {
         driver.get("https://prodoctorov.ru/");
         String title = driver.getTitle();
         Assert.assertEquals("ПроДокторов – сайт отзывов пациентов о врачах №1 в России", title);
-        WebElement inputElement = driver.findElement(By.className("text-field__input"));
-        WebElement submitButton = driver.findElement(By.className("base-search__button"));
+
+        WebElement urlElement = driver.findElement(By.className("b-choose-town-btn-v2"));
+        urlElement.click();
+        WebElement inputElementSearch = driver.findElement(By.className("b-choose-town-popup__search-input"));
+        inputElementSearch.sendKeys("Краснодар");
+        Thread.sleep(2000);
+        WebElement SearchboxElement = driver.findElement(By.className("tt-dataset"));
+        SearchboxElement.click();
+        Thread.sleep(2000);
+
+//      WebElement inputElement = driver.findElement(By.className("text-field__input"));
+        WebElement inputElement = driver.findElement(By.xpath("//input[@placeholder = 'Врачи, клиники, услуги']"));
+        Thread.sleep(2000);
         inputElement.sendKeys("Ницакова Марина Петровна");
+        WebElement submitButton = driver.findElement(By.className("base-search__button"));
         submitButton.click();
         WebElement link = driver.findElement(By.className("b-card__name-link"));
         link.click();

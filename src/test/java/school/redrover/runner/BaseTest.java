8b39b8d2c354b5f5e13887752453acc4d8885de5
Olesya Kolenchenko <kololesya@gmail.com>
2023-04-15 01:35:34 +0200

    add extends BaseTest rename driver -> getDriver() remove Ignore add getWait()

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -8,22 +8,22 @@ import org.testng.annotations.BeforeMethod;
 
 public abstract class BaseTest {
 
-    private WebDriver driver;
+    private WebDriver webDriver;
 
     @BeforeMethod
     protected void beforeMethod() {
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
 
-        driver = new ChromeDriver(chromeOptions);
+        webDriver = new ChromeDriver(chromeOptions);
     }
 
     @AfterMethod
     protected void afterMethod() {
-        driver.quit();
+        webDriver.quit();
     }
 
     protected WebDriver getDriver() {
-        return driver;
+        return webDriver;
     }
 }

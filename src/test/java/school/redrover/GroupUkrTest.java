7f924c1332dc9060736044533fe957e6db5c994c
Alex Shchebetun <Alex.Shchebetun@criterionhcm.com>
2023-04-14 18:02:10 +0300

    Update test. Filling  the form

--- src/test/java/school/redrover/GroupUkrTest.java
+++ src/test/java/school/redrover/GroupUkrTest.java
@@ -1,11 +1,11 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import org.testng.Assert;
 
 import org.testng.annotations.Test;
@@ -74,6 +74,20 @@ public class GroupUkrTest extends BaseTest {
         element.click();
         WebElement header = driver.findElement(By.className("main-header"));
         Assert.assertEquals(header.getText(),"Elements");
+        WebElement textTab = driver.findElement(By.className("text"));
+        textTab.click();
+        WebElement fullNameField = driver.findElement(By.id("userName"));
+        fullNameField.sendKeys("Test User");
+        WebElement emailField = driver.findElement(By.id("userEmail"));
+        emailField.sendKeys("test@mail.com");
+        WebElement addressField = driver.findElement(By.id("currentAddress"));
+        addressField.sendKeys("394 Davis Avenue");
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("arguments[0].scrollIntoView();", submitButton);
+        submitButton.click();
+        WebElement output = driver.findElement(By.id("output"));
+        Assert.assertTrue(output.isDisplayed(),"The for is not displayed");
         driver.quit();
     }
 

23f40829f7fdfb3bc742c529c466f455f7b67235
InnaKushchak <inna.kushchak@gmail.com>
2023-04-14 14:09:12 -0500

    TC_001-TC008Finished.

--- src/test/java/InnaKushchakTest.java
+++ src/test/java/InnaKushchakTest.java
@@ -1,42 +1,77 @@
 import org.openqa.selenium.By;
-
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.time.Duration;
-
 public class InnaKushchakTest {
     private WebDriver driver;
+
     @BeforeMethod
     public void beforeMethod() {
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
-        driver.manage().window().setSize(new Dimension(1920, 1080));
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        driver = new ChromeDriver(chromeOptions);
         driver.get("https://askomdch.com/");
     }
 
     @AfterMethod
+
     public void afterMethod() {
+
         driver.quit();
     }
+
     @Test
-    public void VerifySaleIcon_TC_001()  {
+    public void testVerifySaleIcon() {
         WebElement icon = driver.findElement(By.xpath("//span[@class='onsale']"));
         Assert.assertEquals(icon.getText(), "Sale!");
-
     }
+
     @Test
+    public void validate$SignIsPresent() {
+        WebElement sign = driver.findElement(By.xpath("//span[@class='woocommerce-Price-currencySymbol']"));
+        Assert.assertEquals(sign.getText(), "$");
+    }
 
-    public void OFFOnAllProducts_TC_003()  {
-        String message= driver.findElement(By.xpath("//h3[text()='25% OFF On all products']")).getText();
+    @Test
+    public void offOnAllProducts() {
+
+        String message = driver.findElement(By.xpath("//h3[text()='25% OFF On all products']")).getText();
         Assert.assertEquals(message, "25% OFF On all products");
+    }
 
-   
+    @Test
+    public void verifyingLogInButton() {
+        driver.findElement(By.xpath("//a[@href='https://askomdch.com/account/']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/account/");
+    }
+
+    @Test
+    public void verifyingShopNowButton() {
+        driver.findElement(By.xpath("//a[text()='Shop Now']")).click();
+        Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/store");
+    }
+
+    @Test
+    public void verify5ProductsDisplayed() {
+        Assert.assertEquals(driver.findElement(By.cssSelector("ul.products.columns-5")).findElements(By.tagName("li")).size(), 5);
+    }
+
+    @Test
+    public void manSearchButtonColorVerification() {
+        driver.findElement(By.cssSelector("li[id='menu-item-1228']")).click();
+        Assert.assertEquals(driver.findElement(By.xpath("//button[@value='Search']")).getCssValue("background-color"), "rgba(49, 151, 214, 1)");
+    }
+
+    @Test
+    public void contactUsButtonVerification() {
+        driver.findElement(By.cssSelector("div[class='wp-block-button is-style-fill']")).click();
+        Assert.assertEquals(driver.getTitle(), "Contact Us – AskOmDch");
     }
 }
+

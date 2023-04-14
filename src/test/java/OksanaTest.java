a98b16e5747f748805031edbb347cb5c27994a7e
Turii <o.turij@gmail.com>
2023-04-14 09:55:03 -0700

    Edit First Test Contact Us. TC_010.01

--- src/test/java/OksanaTest.java
+++ src/test/java/OksanaTest.java
@@ -1,20 +1,17 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import runner.BaseTest;
 
-import java.time.Duration;
 
-public class OksanaTest {
+public class OksanaTest extends BaseTest {
+    WebDriver driver;
+
     @Test
-    public void contactUsTest(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com/");
+    public void testContactUs() {
         String text = driver.findElement(By.xpath("//*[@id=\"menu-item-1233\"]/a")).getText();
         Assert.assertEquals(text, "Contact Us");
-        driver.quit();
+
     }
 }

051dd1faf97287acc11a5406f641b43cd8bd8110
new_name <new_email>
2023-04-15 15:53:16 +0200

    My first test

--- src/test/java/OlgaSafTest.java
+++ src/test/java/OlgaSafTest.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class OlgaSafTest {
-}
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import runner.BaseTest;
+
+import java.util.List;
+
+
+    public class OlgaSafTest extends BaseTest {
+
+
+        @Test
+        public void findDollarSignVerificationTest() {
+
+            WebDriver driver = new ChromeDriver();
+
+            driver.get("https://askomdch.com/");
+            WebElement featuredProductsElement = driver.findElement(
+                    By.xpath("//h2[@class='has-text-align-center']"));
+            List<WebElement> featuredProducts = featuredProductsElement.findElements(By.xpath(
+                    "//span[@class='woocommerce-Price-currencySymbol']"));
+            for (WebElement featuredProduct : featuredProducts) {
+                String sign = featuredProduct.getText();
+                Assert.assertEquals(sign, "$");
+            }
+
+            driver.quit();
+        }
+    }
+

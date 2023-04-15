55277621770272397d825ececd6c9a96739f9a8a
Mariia_B <121510822+MCher001@users.noreply.github.com>
2023-04-15 05:48:09 +0300

    Mb/test (#529)
    Change test name.
    Clean code.
    Rename variables.
    Add getDriver methods.

--- src/test/java/school/redrover/GroupFuOpyatJavaTest.java
+++ src/test/java/school/redrover/GroupFuOpyatJavaTest.java
@@ -1,48 +1,29 @@
 package test.java.school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class GroupFuOpyatJavaTest {
+public class GroupFuOpyatJavaTest extends school.redrover.runner.BaseTest {
 
     @Test
-    public void testCheckbox1() throws InterruptedException {
+    public void testNotes(){
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://demoqa.com/");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://demoqa.com/");
+        WebElement textElements = getDriver().findElement(By.xpath("//h5[normalize-space()='Elements']"));
+        textElements.click();
 
-        WebElement textE = driver.findElement(By.xpath("//h5[normalize-space()='Elements']"));
-        textE.click();
+        WebElement textCheckBox = getDriver().findElement(By.xpath("//span[normalize-space()='Check Box']"));
+        textCheckBox.click();
 
-        Thread.sleep(1000);
-
-        WebElement textCB = driver.findElement(By.xpath("//span[normalize-space()='Check Box']"));
-        textCB.click();
-
-        Thread.sleep(1000);
-
-        WebElement buttonE = driver.findElement(By.xpath("//button[@title='Expand all']"));
-        buttonE.click();
+        WebElement buttonExpandAll = getDriver().findElement(By.xpath("//button[@title='Expand all']"));
+        buttonExpandAll.click();
 
         Assert.assertEquals("Notes", "Notes");
 
-        Thread.sleep(1000);
-
-        WebElement buttonC = driver.findElement(By.xpath("//button[@title='Collapse all']"));
-        buttonC.click();
-
-        Thread.sleep(1000);
-
-        driver.quit();
+        WebElement buttonCollapseAll = getDriver().findElement(By.xpath("//button[@title='Collapse all']"));
+        buttonCollapseAll.click();
     }
-
-
 }

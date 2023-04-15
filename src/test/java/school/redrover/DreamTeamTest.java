37c5b308d496f0975f68763d693c99134a32cf05
MarinaVisneacova <127990015+MarinaVisneacova@users.noreply.github.com>
2023-04-15 15:12:49 -0400

    first commit: Update tests (#559)

--- src/test/java/school/redrover/DreamTeamTest.java
+++ src/test/java/school/redrover/DreamTeamTest.java
@@ -1,40 +1,28 @@
 package school.redrover;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class DreamTeamTest extends BaseTest {
-    //First test
+
     @Test
     public void testSecond() throws InterruptedException {
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
-
-        String title = driver.getTitle();
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
+        String title = getDriver().getTitle();
         Assert.assertEquals("Web form", title);
 
-        Thread.sleep(2000);
-
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement textBox = getDriver().findElement(By.name("my-text"));
+        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
 
         textBox.sendKeys("Selenium");
         submitButton.click();
 
-        WebElement message = driver.findElement(By.id("message"));
+        WebElement message = getDriver().findElement(By.id("message"));
         String value = message.getText();
         Assert.assertEquals("Received!", value);
-
-        driver.quit();
     }
 
     @Test
@@ -62,23 +50,17 @@ public class DreamTeamTest extends BaseTest {
 
     @Test
     public void testZoom(){
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://zoom.us");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://zoom.us");
-
-        String title = driver.getTitle();
+        String title = getDriver().getTitle();
         Assert.assertEquals("One platform to connect | Zoom", title);
 
-        WebElement solutionButton = driver.findElement(By.id("btnNewSolutions"));
+        WebElement solutionButton = getDriver().findElement(By.id("btnNewSolutions"));
         solutionButton.click();
 
-        WebElement discoverButton = driver.findElement(By.id("discoverZoom"));
+        WebElement discoverButton = getDriver().findElement(By.id("discoverZoom"));
         String link = discoverButton.getAttribute("href");
         Assert.assertEquals(link, "https://explore.zoom.us/en/industry/");
-        driver.quit();
-
     }
 
 }

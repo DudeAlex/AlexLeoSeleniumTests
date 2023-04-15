ca6153fc4f9b8bee29e7397808e8f72916502bb4
Anna Morozova <morozanna503@gmail.com>
2023-04-15 07:24:37 +0400

    added  refactoring old tests

--- src/test/java/AnnaMorozTest.java
+++ src/test/java/AnnaMorozTest.java
@@ -11,39 +11,28 @@ import java.util.List;
 public class AnnaMorozTest extends BaseTest {
     @Test
     //TC_002.06 Home Page | Featured Products: Validate "$" sign is present under all displayed products
-    public void testSecondOriginal() throws InterruptedException {
-        getDriver().get("https://askomdch.com/");
-        Thread.sleep(3000);
+    public void testSecondOriginal() {
 
         String text = getDriver().findElement(By.cssSelector("h2.has-text-align-center")).getText();
 
         Assert.assertEquals(text,"Featured Products");
 
-        Thread.sleep(3000);
         String symbol = getDriver().findElement(By.cssSelector("span.woocommerce-Price-currencySymbol")).getText();
 
         Assert.assertEquals(symbol,"$");
-
-        getDriver().quit();
     }
 
     @Test
-    public void testOpenShop() throws InterruptedException {
-        getDriver().get("https://askomdch.com/");
-        Thread.sleep(3000);
+    public void testOpenShop() {
 
         getDriver().findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
         String text = getDriver().findElement(By.xpath("//h1[@class='woocommerce-products-header__title page-title']")).getText();
 
         Assert.assertEquals(text,"Store");
-
-        getDriver().quit();
     }
 
     @Test
-    public void testCheckSale() throws InterruptedException {
-        getDriver().get("https://askomdch.com/");
-        Thread.sleep(3000);
+    public void testCheckSale() {
 
         List<WebElement> listFeaturedProducts = getDriver().findElements(By.className("astra-shop-thumbnail-wrap"));
         List<WebElement> listFeaturedProductsText = getDriver().findElements(By.className("astra-shop-summary-wrap"));
@@ -56,15 +45,11 @@ public class AnnaMorozTest extends BaseTest {
     }
 
     @Test
-    public void testCheckContactUs() throws InterruptedException {
-        getDriver().get("https://askomdch.com/");
-        Thread.sleep(3000);
+    public void testCheckContactUs() {
 
         getDriver().findElement(By.xpath("//div[@class ='wp-block-button is-style-fill']")).click();
         String textContact  = getDriver().findElement(By.xpath("//h1[@class ='has-text-align-center']")).getText();
 
         Assert.assertEquals(textContact,"Contact Us");
-
-       getDriver().quit();
     }
 }

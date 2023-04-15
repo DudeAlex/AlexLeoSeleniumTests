12b1223a8803f0b811e2eff3612a76839d2e0d0f
MarinaVisneacova <127990015+MarinaVisneacova@users.noreply.github.com>
2023-04-15 12:47:06 -0400

    first commit zoom (#553)

--- src/test/java/school/redrover/DreamTeamTest.java
+++ src/test/java/school/redrover/DreamTeamTest.java
@@ -35,4 +35,24 @@ public class DreamTeamTest {
 
         driver.quit();
     }
+    @Test
+    public void testZoom(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://zoom.us");
+
+        String title = driver.getTitle();
+        Assert.assertEquals("One platform to connect | Zoom", title);
+
+        WebElement solutionButton = driver.findElement(By.id("btnNewSolutions"));
+        solutionButton.click();
+
+        WebElement discoverButton = driver.findElement(By.id("discoverZoom"));
+        String link = discoverButton.getAttribute("href");
+        Assert.assertEquals(link, "https://explore.zoom.us/en/industry/");
+        driver.quit();
+
+    }
 }

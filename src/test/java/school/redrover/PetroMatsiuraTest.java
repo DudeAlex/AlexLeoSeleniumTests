2b9fea1405ae6db5559d4a1076fc5f256ccbc8df
Peter <matsstang82@gmail.com>
2023-04-14 19:28:46 -0500

    fixed code

--- src/test/java/school/redrover/PetroMatsiuraTest.java
+++ src/test/java/school/redrover/PetroMatsiuraTest.java
@@ -1,39 +1,34 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
 
-public class PetroMatsiuraTest {
+public class PetroMatsiuraTest extends BaseTest {
 
    @Ignore
    @Test
     public void googleStoreTest() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.google.com/");
-        driver.findElement(By.linkText("Store")).click();
-        driver.navigate().refresh();
-        Thread.sleep(5000);
-        Assert.assertEquals(driver.getTitle(), "Google Store for Google Made Devices & Accessories");
-        driver.quit();
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        getDriver().get("https://www.google.com/");
+        getDriver().findElement(By.linkText("Store")).click();
+        getDriver().navigate().refresh();
+        Thread.sleep(3000);
+
+        Assert.assertEquals(getDriver().getTitle(), "Google Store for Google Made Devices & Accessories");
     }
 
     @Test
     public void googleAppsTest() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://www.google.com/");
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        getDriver().get("https://www.google.com/");
+
         String [] result = {
                 "Google Account",
                 "Google",
@@ -45,13 +40,16 @@ public class PetroMatsiuraTest {
                 "Google Meet: Online Video Meetings and Calls | Google Workspace"
         };
         for (int i = 1; i <= result.length; i++) {
-            driver.navigate().refresh();
-            driver.findElement(By.cssSelector("a[aria-label='Google apps']")).click();
-            driver.switchTo().frame("app").findElement(By.xpath("//div[1]/ul/li[" + i + "]")).click();
-            Thread.sleep(5000);
-            Assert.assertEquals(driver.getTitle(), "" + result[i - 1] + "");
-            driver.navigate().back();
+
+            getDriver().navigate().refresh();
+            getDriver().findElement(By.cssSelector("a[aria-label='Google apps']")).click();
+            getDriver().switchTo().frame("app").
+                    findElement(By.xpath("//div[1]/ul/li[" + i + "]")).click();
+            Thread.sleep(3000);
+
+            Assert.assertEquals(getDriver().getTitle(), "" + result[i - 1] + "");
+
+            getDriver().navigate().back();
         }
-        driver.quit();
     }
 }

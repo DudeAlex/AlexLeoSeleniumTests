9560e89b8156ac5a69d342c54cf68418f105018a
mihaibordei <workbordeimihai@gmail.com>
2023-04-14 21:53:19 -0700

    Correct base code

--- src/test/java/MikeBTest.java
+++ src/test/java/MikeBTest.java
@@ -29,65 +29,67 @@ public class MikeBTest {
 
     @Test
 
-    public void testFieldValidation() throws InterruptedException {
+    public void testFieldValidation() {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getTitle(), "AskOmDch – Become a Selenium automation expert!");
 
-        String fieldValidation = driver.findElement(By.xpath("//h2[@class=\"has-text-align-center\"]")).getText();
+        String fieldValidation = driver.findElement(By.xpath("//h2[@class='has-text-align-center']")).getText();
         Assert.assertEquals(fieldValidation, "Featured Products");
-        Thread.sleep(2000);
 
-        String saleTag = driver.findElement(By.xpath("//span[@class=\"onsale\"]")).getText();
+        String saleTag = driver.findElement(By.xpath("//span[@class='onsale']")).getText();
         Assert.assertEquals(saleTag, "Sale!");
     }
 
     @Test
 
-    public void testCurrencyValidation() throws InterruptedException {
+    public void testCurrencyValidation() {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getTitle(), "AskOmDch – Become a Selenium automation expert!");
 
-        String currencyValidation = driver.findElement(By.xpath("//span[@class=\"woocommerce-Price-currencySymbol\"]")).getText();
+        String currencyValidation = driver.findElement(By.xpath("//span[@class='woocommerce-Price-currencySymbol']"))
+                .getText();
         Assert.assertEquals(currencyValidation, "$");
-        Thread.sleep(3000);
+
     }
 
     @Test
 
-    public void testDiscountCheckt() throws InterruptedException {
+    public void testDiscountCheckt() {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getTitle(), "AskOmDch – Become a Selenium automation expert!");
 
-        String discountCheck = driver.findElement(By.xpath("//h3[@class=\"has-text-align-center has-white-color has-text-color has-medium-font-size\"]")).getText();
+        String discountCheck = driver.findElement(
+                By.xpath("//h3[@class='has-text-align-center has-white-color has-text-color has-medium-font-size']"))
+                .getText();
         Assert.assertEquals(discountCheck, "25% OFF On all products");
-        Thread.sleep(3000);
+
     }
 
     @Test
 
-    public void testCheckMenuItem() throws InterruptedException {
+    public void testCheckMenuItem() {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/");
 
         driver.findElement(By.xpath("//li[@id=\"menu-item-1237\"]")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/account/");
-        Thread.sleep(2000);
+
     }
 
     @Test
 
-    public void testValidateWebsitet() throws InterruptedException {
+    public void testValidateWebsitet() {
         driver.manage().window().maximize();
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/");
 
-        driver.findElement(By.xpath("//a[@class=\"wp-block-button__link\"]")).click();
+        driver.findElement(By.xpath("//a[@class='wp-block-button__link']")).click();
         Assert.assertEquals(driver.getCurrentUrl(), "https://askomdch.com/store");
-        Thread.sleep(3000);
+
     }
 
     @Test
@@ -97,7 +99,7 @@ public class MikeBTest {
         driver.get("https://askomdch.com/");
         Assert.assertEquals(driver.getTitle(), "AskOmDch – Become a Selenium automation expert!");
 
-        WebElement allProducts = driver.findElement(By.xpath("//ul[@class=\"products columns-5\"]"));
+        WebElement allProducts = driver.findElement(By.xpath("//ul[@class='products columns-5']"));
         List<WebElement> products = allProducts.findElements(By.tagName("li"));
         Assert.assertEquals(products.size(), 5);
     }

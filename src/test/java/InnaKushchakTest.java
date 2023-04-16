96911f62c16b4d8de485c69adf522f898e0a29dd
natalia <niakunina@cellareye.com>
2023-04-16 16:59:25 -0700

    1. TC_004 and TC_005 were updated, implicitlyWait was removed from tests in YakuninaTest class
    2. A closing curvy bracket was added to Inna Kushchak test, as without it I couldn't check if my updated tests are working

--- src/test/java/InnaKushchakTest.java
+++ src/test/java/InnaKushchakTest.java
@@ -13,14 +13,14 @@ public class InnaKushchakTest extends BaseTest {
         WebElement icon = getDriver().findElement(By.xpath("//span[@class='onsale']"));
         Assert.assertEquals(icon.getText(), "Sale!");
     }
-    
+
     @Test
-    public void  testValidate$SignIsPresent() {
+    public void testValidate$SignIsPresent() {
 
         WebElement sign = getDriver().findElement(By.xpath("//span[@class='woocommerce-Price-currencySymbol']"));
         Assert.assertEquals(sign.getText(), "$");
     }
-    
+
     @Test
     public void testOffOnAllProducts() {
 
@@ -34,63 +34,64 @@ public class InnaKushchakTest extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='https://askomdch.com/account/']")).click();
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://askomdch.com/account/");
     }
-    
+
     @Test
     public void testVerifyingShopNowButton() {
 
         getDriver().findElement(By.xpath("//a[text()='Shop Now']")).click();
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://askomdch.com/store");
     }
-    
+
     @Test
     public void testVerify5ProductsDisplayed() {
 
         Assert.assertEquals(getDriver().findElement(By.cssSelector("ul.products.columns-5")).findElements(By.tagName("li")).size(), 5);
     }
-    
+
     @Test
     public void testSearchButtonColorVerification() {
 
         getDriver().findElement(By.cssSelector("li[id='menu-item-1228']")).click();
         Assert.assertEquals(getDriver().findElement(By.xpath("//button[@value='Search']")).getCssValue("background-color"), "rgba(49, 151, 214, 1)");
     }
-    
+
     @Test
     public void testContactUsButtonVerification() {
 
         getDriver().findElement(By.cssSelector("div[class='wp-block-button is-style-fill']")).click();
         Assert.assertEquals(getDriver().getTitle(), "Contact Us – AskOmDch");
     }
-    
-    @Test
-    public void testWebDriver(){
 
-        WebElement featureProducts=getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
-        List<WebElement> products=featureProducts.findElements(By.tagName("li"));
-        for (WebElement elem:products) {
-         List<WebElement> x= elem.findElements(By.tagName("h2"));
+    @Test
+    public void testWebDriver() {
+
+        WebElement featureProducts = getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
+        List<WebElement> products = featureProducts.findElements(By.tagName("li"));
+        for (WebElement elem : products) {
+            List<WebElement> x = elem.findElements(By.tagName("h2"));
             System.out.println(x.size());
         }
     }
-    
-    @Test
-    public void testBestSellers(){
-            WebElement bestSellers=getDriver().findElement(By.xpath("//ul[@class='product_list_widget']"));
-            List<WebElement> validateProducts= bestSellers.findElements(By.tagName("li"));
-            for(WebElement listOfBest:validateProducts){
 
-            }
+    @Test
+    public void testBestSellers() {
+        WebElement bestSellers = getDriver().findElement(By.xpath("//ul[@class='product_list_widget']"));
+        List<WebElement> validateProducts = bestSellers.findElements(By.tagName("li"));
+        for (WebElement listOfBest : validateProducts) {
+
         }
-        
-    @Test
-    public void testWebDriverSearch(){
+    }
 
-        WebElement featureProducts=getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
-        List<WebElement> products=featureProducts.findElements(By.tagName("li"));
+    @Test
+    public void testWebDriverSearch() {
+
+        WebElement featureProducts = getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
+        List<WebElement> products = featureProducts.findElements(By.tagName("li"));
         products.get(0).getAttribute("baseURI");
         products.get(0).findElement(By.tagName("h2")).getAttribute("textContent");
         products.get(0).findElement(By.tagName("h2")).getAttribute("outerHTML");
-        System.out.println( products.get(0).findElement(By.tagName("h2")).getAttribute("innerHTML"));
+        System.out.println(products.get(0).findElement(By.tagName("h2")).getAttribute("innerHTML"));
+    }
 }
 
 
--- src/test/java/YakuninaTest.java
+++ src/test/java/YakuninaTest.java
@@ -1,9 +1,7 @@
 import org.testng.annotations.Test;
 import org.openqa.selenium.By;
 import runner.BaseTest;
-
 import static org.testng.Assert.assertEquals;
-import java.time.Duration;
 
 public class YakuninaTest extends BaseTest {
 
@@ -12,11 +10,8 @@ public class YakuninaTest extends BaseTest {
     public void testAccountButtonOpensLoginPage() {
         String title = getDriver().getTitle();
         assertEquals(title, "AskOmDch – Become a Selenium automation expert!");
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         getDriver().findElement(By.xpath("//a[@href=\"https://askomdch.com/account/\"][@class=\"menu-link\"]")).click();
         assertEquals("https://askomdch.com/account/", getDriver().getCurrentUrl());
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     }
 
     //TC_005
@@ -24,11 +19,8 @@ public class YakuninaTest extends BaseTest {
     public void testShopNowButtonOpensUpStorePage() {
         String title = getDriver().getTitle();
         assertEquals(title, "AskOmDch – Become a Selenium automation expert!");
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-
         getDriver().findElement(By.cssSelector("a.wp-block-button__link")).click();
         assertEquals("https://askomdch.com/store", getDriver().getCurrentUrl());
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(500));
     }
 }
 

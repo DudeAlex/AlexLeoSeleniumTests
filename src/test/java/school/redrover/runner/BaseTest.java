dd06238c048f37ebd4206db3c7245e20f21d883c
Yallkis <130273141+Yallkis@users.noreply.github.com>
2023-04-16 19:39:34 +0300

    Добавил возможность объявления в BeforeMethod разрешения окна браузера. По умолчанию 1920х1080 (#608)
    Signed-off-by: Yallkis <lostarkkisya@gmail.com>

--- src/test/java/school/redrover/runner/BaseTest.java
+++ src/test/java/school/redrover/runner/BaseTest.java
@@ -9,11 +9,18 @@ import org.testng.annotations.BeforeMethod;
 public abstract class BaseTest {
 
     private WebDriver driver;
+    private Integer resolutionX;
+    private Integer resolutionY;
 
     @BeforeMethod
     protected void beforeMethod() {
         ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        if (resolutionX == null || resolutionY == null){
+            chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        } else {
+            chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=" + resolutionX + "," + resolutionY);
+        }
 
         driver = new ChromeDriver(chromeOptions);
     }
@@ -23,6 +30,10 @@ public abstract class BaseTest {
         driver.quit();
     }
 
+    protected void setResolution(Integer resolutionX,Integer resolutionY) {
+        this.resolutionX = resolutionX;
+        this.resolutionY = resolutionY;
+    }
     protected WebDriver getDriver() {
         return driver;
     }

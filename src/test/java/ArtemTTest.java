a669a140ce0feb218508ffd2e33e7512031a32fa
tymocoderod <tymocoder@gmail.com>
2023-04-16 18:40:00 -0700

    excessive code removed

--- src/test/java/ArtemTTest.java
+++ src/test/java/ArtemTTest.java
@@ -1,36 +1,26 @@
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
+import runner.BaseTest;
 import java.util.List;
-
 import static org.testng.Assert.assertTrue;
-import static org.testng.Assert.fail;
 
-public class ArtemTTest {
+public class ArtemTTest extends BaseTest {
+
     @Test
     public void Test_TC_001_33() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        try {
-            driver.get("https://askomdch.com/");
-            WebElement featuredProducts = driver
-                    .findElement(By.xpath("//*[contains(text(),'Featured Products')]"));
-            List<WebElement> saleProducts = featuredProducts
-                    .findElements(By.xpath("//li[contains(@class, 'ast-article-single')]" +
-                            "[count(.//span[contains(@class, 'woocommerce-Price-currencySymbol')]) > 1]"));
-            for (WebElement saleProduct : saleProducts) {
-                assertTrue(saleProduct.findElement(By.cssSelector("span.onsale"))
-                        .isDisplayed(), "Sale icon not found on a sale product: " + saleProduct.getText());
-            }
-        } catch (Exception e) {
-            fail("An exception occurred: " + e.getMessage());
-        } finally {
-            driver.quit();
-        }
-    }
+        getDriver().get("https://askomdch.com/");
 
+        WebElement featuredProducts = getDriver()
+                .findElement(By.xpath("//*[contains(text(),'Featured Products')]"));
+        List<WebElement> saleProducts = featuredProducts
+                .findElements(By.xpath("//li[contains(@class, 'ast-article-single')]" +
+                        "[count(.//span[contains(@class, 'woocommerce-Price-currencySymbol')]) > 1]"));
+
+        for (WebElement saleProduct : saleProducts) {
+            assertTrue(saleProduct.findElement(By.cssSelector("span.onsale"))
+                    .isDisplayed(), "Sale icon not found on a sale product: " + saleProduct.getText());
+        }
+
+    }
 }

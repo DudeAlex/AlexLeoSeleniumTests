743e7e28c13d52f5874eec94e686aeff5e4af65f
Elena-Mansurova <mansurova.elen@gmail.com>
2023-04-17 19:32:14 +0900

    add new test

--- src/test/java/school/redrover/GroupTwentyThreeTest.java
+++ src/test/java/school/redrover/GroupTwentyThreeTest.java
@@ -1,7 +1,10 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;

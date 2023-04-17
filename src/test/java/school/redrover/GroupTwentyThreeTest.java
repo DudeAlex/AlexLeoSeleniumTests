499a613f271d2d31750347632dbf88ec817d01b3
Elena-Mansurova <mansurova.elen@gmail.com>
2023-04-17 19:25:01 +0900

    add new test

--- src/test/java/school/redrover/GroupTwentyThreeTest.java
+++ src/test/java/school/redrover/GroupTwentyThreeTest.java
@@ -29,4 +29,52 @@ public class GroupTwentyThreeTest {
 
         driver.quit();
     }
+
+    @Test
+    public void testMansurovaElena() throws InterruptedException {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://openweathermap.org/";
+        String cityName = "Paris";
+        String expResult = "Paris, FR";
+
+        driver.get(url);
+
+        Thread.sleep(5000);
+
+        WebElement searchCityField = driver.findElement(
+                By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']")
+        );
+
+        searchCityField.click();
+
+        searchCityField.sendKeys(cityName);
+
+        WebElement searchButton = driver.findElement(
+                By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']")
+        );
+        searchButton.click();
+        Thread.sleep(3000);
+
+        WebElement parisFRChoiceInDropdownMenu = driver.findElement(
+                By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
+        );
+        parisFRChoiceInDropdownMenu.click();
+
+        WebElement h2CityCountryHeader = driver.findElement(
+                By.xpath("//div[@id = 'weather-widget']//h2")
+        );
+        Thread.sleep(2000);
+        String actualResult = h2CityCountryHeader.getText();
+
+        Assert.assertEquals(actualResult, expResult);
+
+        driver.quit();
+    }
+
+
 }
+

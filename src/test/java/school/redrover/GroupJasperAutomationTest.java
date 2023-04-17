f9019faca716e822f762d0c34249c70d1cb64885
alexpyatachenko <59926373+alexpyatachenko@users.noreply.github.com>
2023-04-17 16:34:19 +0300

    Add 2 new tests and delete old. (#657)
    1. Delete DouTest.java
    2. Add testOrderIphone14
    3. Add testNumberOfIphoneModelsAvailable
    4. Fix test(add sleep).

--- src/test/java/school/redrover/GroupJasperAutomationTest.java
+++ src/test/java/school/redrover/GroupJasperAutomationTest.java
@@ -1,13 +1,11 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
@@ -15,7 +13,7 @@ import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 import java.time.Duration;
-import java.util.Base64;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
 
@@ -198,5 +196,72 @@ public class GroupJasperAutomationTest extends BaseTest {
 
         WebElement errorMessage = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@data-test ='error']")));
         Assert.assertEquals(errorMessage.getText(),"Epic sadface: Sorry, this user has been locked out.");
+    }
+
+    @Test
+    public void testOrderIphone14() throws InterruptedException {
+        getDriver().get("https://www.apple.com/");
+        WebElement searchButton = getDriver().findElement(By.id("globalnav-menubutton-link-search"));
+        searchButton.click();
+
+        WebElement searchBox = getDriver().findElement(By.className("globalnav-searchfield-input"));
+        searchBox.sendKeys("IPhone 14");
+        Thread.sleep(3000);
+
+        WebElement chooseIphone = getDriver().findElement(By.xpath("//a[@data-label = 'iPhone 14 Pro and iPhone 14 Pro Max']"));
+        chooseIphone.click();
+
+        WebElement buyIphoneButton = getDriver().findElement(By.xpath("//div[@class = 'section-content']/a[@aria-label = 'Buy iPhone 14 Pro']"));
+        buyIphoneButton.click();
+
+        WebElement chooseVersion = getDriver().findElement(By.xpath("//input[@value = '6_1inch']"));
+        chooseVersion.click();
+
+        WebElement chooseColor = getDriver().findElement(By.xpath("//input[@value = 'deeppurple']/following-sibling::label/img"));
+        chooseColor.click();
+        Thread.sleep(2000);
+
+        WebElement chooseVolume = getDriver().findElement(By.xpath("//input[@value = '128gb']"));
+        chooseVolume.click();
+
+        WebElement isTradeIn = getDriver().findElement(By.xpath("//input[@id = 'noTradeIn']"));
+        isTradeIn.click();
+        Thread.sleep(2000);
+
+        WebElement paymentOption = getDriver().findElement(By.xpath("//span[text() = 'Buy']"));
+        paymentOption.click();
+        Thread.sleep(1000);
+
+        WebElement carrierChoose = getDriver().findElement(By.xpath("//span[text() = 'Connect to any carrier later']"));
+        carrierChoose.click();
+        Thread.sleep(3000);
+
+        WebElement addCoverage = getDriver().findElement(By.xpath("//input[@id = 'applecareplus_58_noapplecare']"));
+        addCoverage.click();
+        Thread.sleep(1000);
+
+        WebElement addToCartButton = getDriver().findElement(By.xpath("//button[@value = 'add-to-cart']"));
+        addToCartButton.click();
+
+        WebElement reviewBagButton = getDriver().findElement(By.xpath("//button[@name = 'proceed']"));
+        reviewBagButton.click();
+
+        WebElement orderedProductName = getDriver().findElement(By.linkText("iPhone 14 Pro 128GB Deep Purple"));
+        Assert.assertEquals(orderedProductName.getText(), "iPhone 14 Pro 128GB Deep Purple");
+    }
+
+    @Test
+    public void testNumberOfIphoneModelsAvailable(){
+        getDriver().get("https://www.apple.com/");
+        WebElement searchButton = getDriver().findElement(By.id("globalnav-menubutton-link-search"));
+        searchButton.click();
+
+        WebElement iPhonePage = getDriver().findElement(By.xpath("//a[@aria-label = 'iPhone']"));
+        iPhonePage.click();
+
+        WebElement listOfIphones = getDriver().findElement(By.cssSelector("ul.chapternav-items"));
+        List<WebElement> list = listOfIphones.findElements(By.xpath("li[contains(@class, 'item-iphone-')]"));
+
+        Assert.assertEquals(list.size(), 5);
     }
 }

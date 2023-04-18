f28d58b4cbeafb12b7b459d1eb994bc3732126c2
InnaKushchak <inna.kushchak@gmail.com>
2023-04-17 21:23:55 -0500

    TC_001-TC008+ListPractice2

--- src/test/java/InnaKushchakTest.java
+++ src/test/java/InnaKushchakTest.java
@@ -3,7 +3,6 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
-
 import java.util.List;
 
 public class InnaKushchakTest extends BaseTest {
@@ -13,75 +12,75 @@ public class InnaKushchakTest extends BaseTest {
         WebElement icon = getDriver().findElement(By.xpath("//span[@class='onsale']"));
         Assert.assertEquals(icon.getText(), "Sale!");
     }
+
     @Test
-    public void  testValidate$SignIsPresent() {
+    public void testValidate$SignIsPresent() {
 
         WebElement sign = getDriver().findElement(By.xpath("//span[@class='woocommerce-Price-currencySymbol']"));
         Assert.assertEquals(sign.getText(), "$");
     }
+
     @Test
     public void testOffOnAllProducts() {
 
         String message = getDriver().findElement(By.xpath("//h3[text()='25% OFF On all products']")).getText();
         Assert.assertEquals(message, "25% OFF On all products");
     }
+
     @Test
     public void testVerifyingLogInButton() {
 
         getDriver().findElement(By.xpath("//a[@href='https://askomdch.com/account/']")).click();
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://askomdch.com/account/");
     }
+
     @Test
     public void testVerifyingShopNowButton() {
 
         getDriver().findElement(By.xpath("//a[text()='Shop Now']")).click();
         Assert.assertEquals(getDriver().getCurrentUrl(), "https://askomdch.com/store");
     }
+
     @Test
     public void testVerify5ProductsDisplayed() {
 
         Assert.assertEquals(getDriver().findElement(By.cssSelector("ul.products.columns-5")).findElements(By.tagName("li")).size(), 5);
     }
+
     @Test
     public void testSearchButtonColorVerification() {
 
         getDriver().findElement(By.cssSelector("li[id='menu-item-1228']")).click();
         Assert.assertEquals(getDriver().findElement(By.xpath("//button[@value='Search']")).getCssValue("background-color"), "rgba(49, 151, 214, 1)");
     }
+
     @Test
     public void testContactUsButtonVerification() {
 
         getDriver().findElement(By.cssSelector("div[class='wp-block-button is-style-fill']")).click();
         Assert.assertEquals(getDriver().getTitle(), "Contact Us – AskOmDch");
     }
-    @Test
-    public void testWebDriver(){
 
-        WebElement featureProducts=getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
-        List<WebElement> products=featureProducts.findElements(By.tagName("li"));
-        for (WebElement elem:products) {
-         List<WebElement> x= elem.findElements(By.tagName("h2"));
-            System.out.println(x.size());
-        }
+    @Test
+    public void testNumberOfElementInFutureProducts() {
+
+        WebElement featureProducts = getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
+        List<WebElement> products = featureProducts.findElements(By.tagName("li"));
+
+        Assert.assertEquals(products.size(), 5);
     }
-    @Test
-    public void testBestSellers(){
-            WebElement bestSellers=getDriver().findElement(By.xpath("//ul[@class='product_list_widget']"));
-            List<WebElement> validateProducts= bestSellers.findElements(By.tagName("li"));
-            for(WebElement listOfBest:validateProducts){
 
-            }
-        }
     @Test
-    public void testWebDriverSearch(){
+    public void testWebDriverSearchBlueShoes() {
 
-        WebElement featureProducts=getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
-        List<WebElement> products=featureProducts.findElements(By.tagName("li"));
+        WebElement featureProducts = getDriver().findElement(By.xpath("//div[@class='wp-block-group alignfull']//ul"));
+        List<WebElement> products = featureProducts.findElements(By.tagName("li"));
         products.get(0).getAttribute("baseURI");
         products.get(0).findElement(By.tagName("h2")).getAttribute("textContent");
         products.get(0).findElement(By.tagName("h2")).getAttribute("outerHTML");
-        System.out.println( products.get(0).findElement(By.tagName("h2")).getAttribute("innerHTML"));
-}
+
+        Assert.assertEquals(products.get(0).findElement(By.tagName("h2")).getAttribute("innerHTML"), "Blue Shoes");
+    }
 }
 
 

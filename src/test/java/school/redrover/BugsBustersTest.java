f8e505728df05d015442b1992b3838f187418b59
Gelika <65321154+GelikaUS@users.noreply.github.com>
2023-04-17 23:28:26 -0400

    Fix tests, add new tests (#722)

--- src/test/java/school/redrover/BugsBustersTest.java
+++ src/test/java/school/redrover/BugsBustersTest.java
@@ -2,82 +2,63 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
 public class BugsBustersTest extends BaseTest {
+
     @Ignore
     @Test
     public void testAngelinaGiftCardSelection() {
+        getDriver().get("https://www.broadway.com/");
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.broadway.com/");
-
-        WebElement giftCardTab = driver.findElement(By.linkText("Gift Cards"));
+        WebElement giftCardTab = getDriver().findElement(By.linkText("Gift Cards"));
         giftCardTab.click();
 
-        WebElement buyNowButton = driver.findElement(By.linkText("Buy Now"));
+        WebElement buyNowButton = getDriver().findElement(By.linkText("Buy Now"));
         buyNowButton.click();
 
-//        WebElement continueToDeliveryButton = driver.findElement(By.xpath("//button[@type='submit']"));
-//        continueToDeliveryButton.click();
-//
-//        WebElement text = driver.findElement(By.xpath("//div[@data-qa='subtotal-amount']//strong"));
-//        Assert.assertEquals(text.getText(),"$100.00");
+        WebElement continueToDeliveryButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
+        continueToDeliveryButton.click();
 
-        driver.quit();
+        WebElement text = getDriver().findElement(By.xpath("//div[@data-qa='subtotal-amount']//strong"));
+
+        Assert.assertEquals(text.getText(),"$100.00");
     }
 
     @Test
     public void testSergeyConvert() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://www.unitconverters.net/weight-and-mass/lbs-to-kg.htm");
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.unitconverters.net/weight-and-mass/lbs-to-kg.htm");
-
-        WebElement textBox = driver.findElement(By.cssSelector("input#ucfrom.ucdcinput"));
+        WebElement textBox = getDriver().findElement(By.cssSelector("input#ucfrom.ucdcinput"));
         textBox.sendKeys("1");
 
-        WebElement convert = driver.findElement(By.cssSelector("input.ucdcsubmit"));
+        WebElement convert = getDriver().findElement(By.cssSelector("input.ucdcsubmit"));
         convert.click();
 
-        WebElement text = driver.findElement(By.cssSelector("div#ucresult"));
-        Assert.assertEquals(text.getText(), "Result: 1 pound = 0.45359237 kilogram");
+        WebElement text = getDriver().findElement(By.cssSelector("div#ucresult"));
 
-        driver.quit();
+        Assert.assertEquals(text.getText(), "Result: 1 pound = 0.45359237 kilogram");
     }
 
     @Ignore
     @Test
     public void testAngelinaSearchResults() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://www.broadway.com/");
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.get("https://www.broadway.com/");
-
-        WebElement searchBox = driver.findElement(By.id("nav-typeahead-js"));
+        WebElement searchBox = getDriver().findElement(By.id("nav-typeahead-js"));
         searchBox.sendKeys("the phantom of the opera\n");
 
-        WebElement text = driver.findElement(By.xpath("//a[@href=\"/shows/the-phantom-of-the-opera/\"]/h3"));
-        Assert.assertEquals(text.getText(), "The Phantom of the Opera");
+        WebElement text = getDriver().findElement(By.xpath("//a[@href=\"/shows/the-phantom-of-the-opera/\"]/h3"));
 
-        driver.quit();
+        Assert.assertEquals(text.getText(), "The Phantom of the Opera");
     }
 
     @Test
     public void testGetSeleniumWebDriverPageTitle(){
-
         getDriver().get("https://www.selenium.dev/");
 
         WebElement readMoreButton = getDriver().findElement(By.xpath("//a[@href='/documentation/webdriver/']"));
@@ -92,29 +73,38 @@ public class BugsBustersTest extends BaseTest {
     }
 
     @Test
-    public void testMS() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+    public void testMarynaSTitle() {
+        getDriver().get("https://www.funartcolorado.com/");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().maximize();
+        Assert.assertEquals(getDriver().getTitle(), "Home | Funart");
+    }
 
-        driver.get("https://www.funartcolorado.com/");
-        Assert.assertEquals(driver.getTitle(), "Home | Funart");
+    @Test
+    public void testMarynaSArtTab() {
+        getDriver().get("https://www.funartcolorado.com/");
 
-        WebElement artTab = driver.findElement(By.id("comp-jnlr6x4r1label"));
+        WebElement artTab = getDriver().findElement(By.id("comp-jnlr6x4r1label"));
         artTab.click();
-        Thread.sleep(2000);
-        WebElement mosaicArtHeader = driver.findElement(By.cssSelector("div[id='comp-jbrwxjbr'] span"));
-        Assert.assertEquals(mosaicArtHeader.getText(), "About my art:");
 
-        WebElement classesTab = driver.findElement(By.id("comp-jnlr6x4r2label"));
+        WebElement mosaicArtHeader = getDriver().findElement(By.cssSelector("div[id='comp-jbrwxjbr'] span"));
+
+        Assert.assertEquals(mosaicArtHeader.getText(), "About my art:");
+    }
+
+    @Test
+    public void testMarynaSHeader() throws InterruptedException {
+        getDriver().get("https://www.funartcolorado.com/");
+
+        WebElement artTab = getDriver().findElement(By.id("comp-jnlr6x4r1label"));
+        artTab.click();
+
+        WebElement classesTab = getDriver().findElement(By.id("comp-jnlr6x4r2label"));
         classesTab.click();
         Thread.sleep(2000);
-        WebElement classesHeader = driver.findElement(By.cssSelector("h1[class='font_0'] span"));
-        Assert.assertEquals(classesHeader.getText(), "ONLINE course \"Fun Art with Tempered glass\"");
 
-        driver.quit();
+        WebElement classesHeader = getDriver().findElement(By.cssSelector("h1[class='font_0'] span"));
+
+        Assert.assertEquals(classesHeader.getText(), "ONLINE course \"Fun Art with Tempered glass\"");
     }
 
     @Ignore
@@ -137,82 +127,75 @@ public class BugsBustersTest extends BaseTest {
 
     @Ignore
     @Test
-    public void testMsArtMosaicVideo() throws InterruptedException {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080", "−−incognito");
+    public void testMarynaSArtMosaicVideoHeader() {
+        getDriver().get("https://www.funartcolorado.com/");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
+        WebElement mosaicArtHeader = getDriver().findElement(By.xpath("//p[@id='comp-jnlr6x4r2label']"));
 
-        driver.get("https://www.funartcolorado.com/");
-
-        WebElement mosaicArtHeader = driver.findElement(By.xpath("//p[@id='comp-jnlr6x4r2label']"));
         Assert.assertEquals(mosaicArtHeader.getText(), "Classes");
+    }
 
-        WebElement classesTab = driver.findElement(By.id("comp-jnlr6x4r2label"));
+    @Ignore
+    @Test
+    public void testMarynaSArtMosaicVideo() throws InterruptedException {
+        getDriver().get("https://www.funartcolorado.com/");
+
+        WebElement classesTab = getDriver().findElement(By.id("comp-jnlr6x4r2label"));
         classesTab.click();
         Thread.sleep(2000);
-        WebElement playbutton = driver.findElement(By.xpath("//div[@class='QzC0oQ E1QW47']//*[name()='svg']"));
-        playbutton.click();
-        WebElement video = driver.findElement(By.tagName("video"));
-        Thread.sleep(3000);
-        Assert.assertTrue(Float.parseFloat(video.getAttribute("currentTime")) > 0);
 
-        driver.quit();
+        WebElement playButton = getDriver().findElement(By.xpath("//div[@class='QzC0oQ E1QW47']//*[name()='svg']"));
+        playButton.click();
+
+        WebElement video = getDriver().findElement(By.tagName("video"));
+        Thread.sleep(3000);
+
+        Assert.assertTrue(Float.parseFloat(video.getAttribute("currentTime")) > 0);
     }
 
     @Test
     public void testMarynaLFirstTest() {
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        getDriver().get("https://invoice-generator.com/");
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://invoice-generator.com/");
-
-
-        WebElement signInButton = driver.findElement(By.xpath("/html/body/nav[1]/div/div/ul/li[3]/a"));
+        WebElement signInButton = getDriver().findElement(By.xpath("/html/body/nav[1]/div/div/ul/li[3]/a"));
         signInButton.click();
 
-
-        WebElement emailAddress = driver.findElement(By.xpath("//*[@id='inputEmail']"));
+        WebElement emailAddress = getDriver().findElement(By.xpath("//*[@id='inputEmail']"));
         emailAddress.sendKeys("victoria.bilanko@gmail.com");
 
-        WebElement password = driver.findElement(By.xpath("//*[@id='inputPassword']"));
+        WebElement password = getDriver().findElement(By.xpath("//*[@id='inputPassword']"));
         password.sendKeys("Java12345@");
 
-        WebElement signInBtn = driver.findElement(By.xpath("/html/body/div/div/div[2]/form/button"));
+        WebElement signInBtn = getDriver().findElement(By.xpath("/html/body/div/div/div[2]/form/button"));
         signInBtn.click();
 
-        driver.quit();
+        WebElement userName = getDriver().findElement(By.xpath("//a[contains(text(), 'JavaTest')]"));
+
+        Assert.assertEquals(userName.getText(), "JavaTest");
     }
 
     @Test
     public void testAnastasiyaFirstTest () {
+        getDriver().get("https://demoqa.com/text-box");
 
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://demoqa.com/text-box");
-
-        WebElement firstNameTextBox = driver.findElement(By.id("userName"));
+        WebElement firstNameTextBox = getDriver().findElement(By.id("userName"));
         firstNameTextBox.sendKeys("First Last");
 
-        WebElement emailTextBox = driver.findElement(By.xpath("//*[@id=\"userEmail\"]"));
+        WebElement emailTextBox = getDriver().findElement(By.xpath("//*[@id=\"userEmail\"]"));
         emailTextBox.sendKeys("blablabla@gmail.com");
 
-        WebElement currentAddress = driver.findElement(By.xpath("//*[@id=\"currentAddress\"]"));
+        WebElement currentAddress = getDriver().findElement(By.xpath("//*[@id=\"currentAddress\"]"));
         currentAddress.sendKeys("2023 New Year St, NY");
 
-        WebElement permanentAddress = driver.findElement(By.xpath("//*[@id=\"permanentAddress\"]"));
+        WebElement permanentAddress = getDriver().findElement(By.xpath("//*[@id=\"permanentAddress\"]"));
         permanentAddress.sendKeys("2024 New Year St, NY");
 
-        WebElement submitButton = driver.findElement(By.id("submit"));
+        WebElement submitButton = getDriver().findElement(By.id("submit"));
         submitButton.click();
 
-        WebElement nameText = driver.findElement(By.xpath("//*[@id=\"name\"]"));
-        Assert.assertEquals(nameText.getText(), "Name:First Last");
+        WebElement nameText = getDriver().findElement(By.xpath("//*[@id=\"name\"]"));
 
-        driver.quit();
+        Assert.assertEquals(nameText.getText(), "Name:First Last");
     }
 
     @Ignore
@@ -223,8 +206,7 @@ public class BugsBustersTest extends BaseTest {
 
         getDriver().get("https://www.ohiofamilypractice.com/");
         Thread.sleep(1000);
-        WebElement servicesReference = getDriver().findElement(By.xpath(
-                "//a[@href='/services'][@class='Header-nav-item']"));
+        WebElement servicesReference = getDriver().findElement(By.xpath("//a[@href='/services'][@class='Header-nav-item']"));
         servicesReference.click();
         Thread.sleep(1000);
         WebElement h1 = getDriver().findElement(By.xpath("//h1"));
@@ -236,8 +218,7 @@ public class BugsBustersTest extends BaseTest {
 
     @Ignore
     @Test
-    public void testMariaAmazonSearch() throws InterruptedException {
-
+    public void testMariaAmazonSearch() {
         getDriver().get("https://www.amazon.com/");
 
         WebElement searchBox = getDriver().findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
@@ -250,8 +231,7 @@ public class BugsBustersTest extends BaseTest {
     }
 
     @Test
-    public void testMariaLogin() throws InterruptedException {
-
+    public void testMariaLogin() {
         getDriver().get("https://www.saucedemo.com/");
 
         WebElement userName = getDriver().findElement(By.cssSelector("input[placeholder='Username']"));
@@ -264,9 +244,26 @@ public class BugsBustersTest extends BaseTest {
         loginButton.click();
 
         WebElement header = getDriver().findElement(By.cssSelector("div[class='app_logo']"));
+
         Assert.assertEquals(header.getText(), "Swag Labs");
+    }
 
+    @Test
+    public void testAngelinaReservationButtonTitle() {
+        getDriver().get("https://nailspaas.com/");
 
+        String title = getDriver().getTitle();
+
+        Assert.assertEquals(title, "Nail Spa A&S");
+    }
+
+    @Test
+    public void testAngelinaReservationButton() {
+        getDriver().get("https://nailspaas.com/");
+
+        WebElement reservationButton = getDriver().findElement(By.xpath("//a[contains(text(),'Reservation')]"));
+
+        Assert.assertEquals(reservationButton.getText(), "Reservation");
     }
 }
 

e6a7ec64b28bf9ba722a8c501e6d42c0af0b018e
Stoyana <101792053+StoyanaDi@users.noreply.github.com>
2023-04-18 19:23:25 +0300

    refactoring tests for sorting price (#740)

--- src/test/java/school/redrover/GroupOlesyaTests.java
+++ src/test/java/school/redrover/GroupOlesyaTests.java
@@ -19,6 +19,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 public class GroupOlesyaTests extends BaseTest {
     private final String URL = "https://www.saucedemo.com/";
     private final String LOGIN = "standard_user";
@@ -54,7 +56,7 @@ public class GroupOlesyaTests extends BaseTest {
 
     public List<String> productNames(){
         List<WebElement> el = getDriver().findElements(By.xpath("//div[@class = 'inventory_item_name']"));
-        return el.stream().map(WebElement::getText).collect(Collectors.toList());
+        return el.stream().map(WebElement::getText).collect(toList());
     }
 
     public void sortElements(String visibleText){
@@ -89,7 +91,7 @@ public class GroupOlesyaTests extends BaseTest {
     public List <String> getListOfItemInCart(){
         WebElement cartList = getDriver().findElement(By.className("cart_list"));
         List<WebElement> cartItems = cartList.findElements(By.className("inventory_item_name"));
-        return cartItems.stream().map(WebElement::getText).collect(Collectors.toList());
+        return cartItems.stream().map(WebElement::getText).collect(toList());
     }
     private void goToAllItemsInBurgerMenu() {
         WebElement allItemsLink = getDriver().findElement(By.id("inventory_sidebar_link"));
@@ -97,34 +99,15 @@ public class GroupOlesyaTests extends BaseTest {
         allItemsLink.click();
     }
 
-    public List<Double> getListBeforeSorting(String sortName) {
-        List<WebElement> beforeFilterPrice = getDriver().findElements(By.className("inventory_item_price"));
-        List<Double> beforeFilterPriceList = new ArrayList<>();
+    public List<Double> listOfPrice() {
 
-        for (WebElement e : beforeFilterPrice) {
-            beforeFilterPriceList.add(Double.valueOf(e.getText().replace("$", "")));
-        }
-
-        WebElement funnelIcon = getDriver().findElement(By.className("select_container"));
-        funnelIcon.click();
-
-        Select drpOrder = new Select(getDriver().findElement(By.className("product_sort_container")));
-        drpOrder.selectByVisibleText(sortName);
-        Collections.sort(beforeFilterPriceList);
-
-        return beforeFilterPriceList;
+        return getListItems(By.className("inventory_item_price"))
+                .stream()
+                .map(WebElement::getText)
+                .map(n -> n.replace("$", ""))
+                .map(s -> Double.parseDouble(s))
+                .collect(toList());
     }
-
-    public List<Double> getListAfterSorting() {
-        List<WebElement> afterFilterPrice = getDriver().findElements(By.className("inventory_item_price"));
-        List<Double> afterFilterPriceList = new ArrayList<>();
-
-        for (WebElement e : afterFilterPrice) {
-            afterFilterPriceList.add(Double.valueOf(e.getText().replace("$", "")));
-        }
-        return afterFilterPriceList;
-    }
-
 
     @Test
     public void standardUserLoginTest() {
@@ -134,7 +117,7 @@ public class GroupOlesyaTests extends BaseTest {
     }
 
     public List<String> getTextList (List<WebElement> list) {
-        return list.stream().map(WebElement::getText).collect(Collectors.toList());
+        return list.stream().map(WebElement::getText).collect(toList());
     }
 
     public void clickOnEachElement (List<WebElement> list) {
@@ -195,18 +178,27 @@ public class GroupOlesyaTests extends BaseTest {
     @Test
     public void sortingByPriceLowToHighTest() {
         loginToSite(LOGIN);
-        List<Double> expectedResult = getListBeforeSorting("Price (low to high)");
-        Assert.assertEquals(getListAfterSorting(), expectedResult);
-        getDriver().quit();
+
+        List<Double> pricesBeforeSort = listOfPrice();
+        Collections.sort(pricesBeforeSort);
+
+        sortElements("Price (low to high)");
+        List<Double> pricesAfterSort = listOfPrice();
+
+        Assert.assertEquals(pricesAfterSort, pricesBeforeSort);
     }
 
     @Test
     public void sortingByPriceHighToLowTest() {
         loginToSite(LOGIN);
-        List<Double> expectedResult = getListBeforeSorting("Price (high to low)");
-        Collections.reverse(expectedResult);
-        Assert.assertEquals(getListAfterSorting(), expectedResult);
-        getDriver().quit();
+
+        List<Double> pricesBeforeSort = listOfPrice();
+        pricesBeforeSort.sort(Collections.reverseOrder());
+
+        sortElements("Price (high to low)");
+        List<Double> pricesAfterSort = listOfPrice();
+
+        Assert.assertEquals(pricesAfterSort, pricesBeforeSort);
     }
 
     @Test

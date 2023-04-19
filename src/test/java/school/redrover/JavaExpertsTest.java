636803951d17bf4b58eeeac7c834be04819b111d
AlekseiChapaev <93160447+AlekseiChapaev@users.noreply.github.com>
2023-04-19 17:12:21 +0200

    refactor class code (#808)
    * refactor checkItemsTest and checkSortingTest, create selectTypeOfSortingItems method
    * Ignore testLoginUser
    * Ignore testRegisterUser

--- src/test/java/school/redrover/JavaExpertsTest.java
+++ src/test/java/school/redrover/JavaExpertsTest.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 
@@ -36,6 +37,7 @@ public class JavaExpertsTest extends BaseTest {
         assertEquals("Received!", value, "Form submit is fail");
     }
 
+    @Ignore
     @Test
     public void testRegisterUser(){
         getDriver().get("http://selenium1py.pythonanywhere.com/en-gb/accounts/login/");
@@ -55,6 +57,7 @@ public class JavaExpertsTest extends BaseTest {
                 "User registration is fail");
     }
 
+    @Ignore
     @Test(dependsOnMethods = "testRegisterUser")
     public void testLoginUser(){
         getDriver().get("http://selenium1py.pythonanywhere.com/en-gb/accounts/login/");

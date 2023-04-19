da1c968b60b189a6549aa87e7d33ce48fd39cc49
Nadi-Olesi <n.i.olesiyuk@gmail.com>
2023-04-19 15:59:25 +0200

    new test login dev

--- src/test/java/school/redrover/OrthazoneLoginLogoutTest.java
+++ src/test/java/school/redrover/OrthazoneLoginLogoutTest.java
@@ -28,4 +28,22 @@ public class OrthazoneLoginLogoutTest extends BaseTest{
         WebElement textOnLogoutPage = getDriver().findElement(By.xpath("//div[@class='asteps__head']"));
         Assert.assertEquals(textOnLogoutPage.getText(),"ACCOUNT LOGOUT");
     }
+
+    @Test
+    public void testLoginDev () throws InterruptedException {
+
+        getDriver().get("https://devortha.dentazone.com/");
+
+        WebElement account = getDriver().findElement(By.xpath("//button[@title='Your account menu']//span[@class='y-btn__txt']"));
+        account.click();
+        Thread.sleep(1000);
+
+        getDriver().findElement(By.xpath("//div[@class=\"y-modal__main-inner\"]//span[@class='y-btn__txt' and text()='Login']")).click();
+        getDriver().findElement(By.name("email")).sendKeys("ntest@orthazone.com");
+        getDriver().findElement(By.name("password")).sendKeys("123456789");
+        getDriver().findElement(By.xpath("//button[@type='submit'][@class='r-btn abtn' and text() = 'Login']")).click();
+
+        WebElement accountName = getDriver().findElement(By.xpath("//div[@class='y-header__user']//span[3]"));
+        Assert.assertEquals(accountName.getText(), "Ntest");
+    }
 }

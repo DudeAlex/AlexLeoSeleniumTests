3e67c1574e1d46b778d8cff81a803467c7926841
VViktoriia <viktoriia.vylobkova@gmail.com>
2023-04-19 15:17:53 -0700

    Update tests

--- src/test/java/VViktoriaTest.java
+++ src/test/java/VViktoriaTest.java
@@ -3,37 +3,27 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import runner.BaseTest;
 
 import java.net.URL;
 
-public class VViktoriaTest {
+public class VViktoriaTest extends BaseTest {
     @Test
-    public void buttonOpened_TC_009(){
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com/");
+    public void testButtonOpened_TC_009(){
         String url = "https://askomdch.com/store";
+        getDriver().findElement(By.cssSelector("div.wp-block-button>a[href=\"/store\"]")).click();
 
-        driver.findElement(By.cssSelector("div.wp-block-button>a[href=\"/store\"]")).click();
         Assert.assertEquals(url, "https://askomdch.com/store");
-        driver.quit();
     }
-
     @Test
-    public void searhButton_TC_009_01() {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://askomdch.com/");
-
+    public void testSearhButton_TC_009_01() {
         String wordOfSearch = "jea";
-
-        driver.findElement(By.cssSelector("div.wp-block-button>a[href=\"/store\"]")).click();
-        driver.findElement(By.id("woocommerce-product-search-field-0")).click();
-        driver.findElement(By.id("woocommerce-product-search-field-0")).sendKeys(wordOfSearch);
-        driver.findElement(By.cssSelector("[type='submit']")).click();
-        driver.findElement(By.cssSelector("[class=\"products columns-4\"]")).isDisplayed();
+        getDriver().findElement(By.cssSelector("div.wp-block-button>a[href=\"/store\"]")).click();
+        getDriver().findElement(By.id("woocommerce-product-search-field-0")).click();
+        getDriver().findElement(By.id("woocommerce-product-search-field-0")).sendKeys(wordOfSearch);
+        getDriver().findElement(By.cssSelector("[type='submit']")).click();
+        getDriver().findElement(By.cssSelector("[class=\"products columns-4\"]")).isDisplayed();
 
         Assert.assertSame("jea", wordOfSearch);
-        driver.quit();
     }
 }

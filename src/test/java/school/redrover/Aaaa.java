777ec40191c3c3ac0f1186e06c97aa7404b49afc
davidmav19 <107568567+davidmav19@users.noreply.github.com>
2023-04-20 18:45:00 -0400

    Sd/add base class (#780)
    * Before method
    
    * extends BaseTest
    
    * extends BaseTest
    
    * extends BaseTest
    
    * Fixed BaseTest
    
    * Fixed BaseTest

---
+++ src/test/java/school/redrover/Aaaa.java
@@ -0,0 +1,4 @@
+package school.redrover;
+
+public class Aaaa {
+}
--- src/test/java/school/redrover/DavidMTest.java
+++ src/test/java/school/redrover/DavidMTest.java
@@ -1,68 +1,45 @@
 package school.redrover;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-public class DavidMTest {
-    private ChromeOptions chromeOptions;
-    private WebDriver driver;
-
-    @BeforeMethod
-    private void beforeMethod(){
-        chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
-
-        driver = new ChromeDriver(chromeOptions);
-    }
-    @AfterMethod
-    private void afterMethod(){
-        driver.quit();
-    }
-
+import school.redrover.runner.BaseTest;
+public class DavidMTest extends BaseTest {
     @Test
     public void testFirst() throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.google.com");
-        WebElement textBox = driver.findElement(By.name("q"));
+        getDriver().get("https://www.google.com");
+        WebElement textBox = getDriver().findElement(By.name("q"));
 
         textBox.sendKeys("selenium");
         textBox.sendKeys(Keys.RETURN);
 
         Thread.sleep(2000);
 
-        WebElement text = driver.findElement(By.xpath("//h3[text() = 'Selenium']"));
+        WebElement text = getDriver().findElement(By.xpath("//h3[text() = 'Selenium']"));
         Assert.assertEquals(text.getText(),"Selenium");
 
          }
-
     @Test
     public void testSecond() throws InterruptedException {
 
-        WebDriver driver = new ChromeDriver(chromeOptions);
-        driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        getDriver().get("https://www.selenium.dev/selenium/web/web-form.html");
 
-        String title = driver.getTitle();
+        String title = getDriver().getTitle();
         Assert.assertEquals("Web form", title);
 
         Thread.sleep(2000);
 
-        WebElement textBox = driver.findElement(By.name("my-text"));
-        WebElement submitButton = driver.findElement(By.cssSelector("button"));
+        WebElement textBox = getDriver().findElement(By.name("my-text"));
+        WebElement submitButton = getDriver().findElement(By.cssSelector("button"));
 
         textBox.sendKeys("Selenium");
         submitButton.click();
 
-        WebElement message = driver.findElement(By.id("message"));
+        WebElement message = getDriver().findElement(By.id("message"));
         String value = message.getText();
         Assert.assertEquals("Received!", value);
     }
     }
+

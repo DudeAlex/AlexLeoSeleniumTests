27e3eb8ce3b9bbf36a79ef0f3274d717b9150cbf
Aliaksandra Anufryieva <129998514+AliaksandraAnufryieva@users.noreply.github.com>
2023-04-20 12:25:04 -0400

    AA/group google_it Add new tests (#852)
     Add new tests: testWikipediaSearch, testWikipediaLanguage,
    testFreeFormatterXPath.
    Delete testJenkinsTitle.

--- src/test/java/school/redrover/AliaksandraAnufryievaTest.java
+++ src/test/java/school/redrover/AliaksandraAnufryievaTest.java
@@ -4,42 +4,86 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.runner.BaseTest;
 import java.time.Duration;
 
 public class AliaksandraAnufryievaTest extends BaseTest {
 
-    @Ignore
     @Test
-    public void testEtsy() {
+    public void testWikipediaSearch() {
+        getDriver().get("https://www.wikipedia.org/");
+
+        WebElement search = getDriver().findElement(By.name("search"));
+        search.sendKeys("Jenkins");
+        search.sendKeys(Keys.RETURN);
+
+        WebElement text = getDriver().findElement(By.id("firstHeading"));
+
+        Assert.assertEquals(text.getText(), "Jenkins");
+    }
+
+    @Test
+    public void testWikipediaLanguage() {
+        getDriver().get("https://www.wikipedia.org/");
+
+        WebElement languageButton = getDriver().findElement(By.className("lang-list-button"));
+        languageButton.click();
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+        WebElement chooseBelLanguage = getDriver().findElement(By.xpath("//a[@title='Belaruskaya']"));
+        chooseBelLanguage.click();
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
+
+        Assert.assertEquals(getDriver().getTitle(), "Вікіпедыя");
+    }
+
+    @Test
+    public void testFreeFormatterXPath() {
+        getDriver().get("https://freeformatter.com/");
+
+        WebElement xpathTesterSearch = getDriver().findElement(By.xpath("//a[@title='XPath Tester']"));
+        xpathTesterSearch.click();
+
+        WebElement textXML = getDriver().findElement(By.xpath("//textarea[@placeholder='Copy-paste your XML here']"));
+        textXML.sendKeys("<title lang='en'>Harry Potter</title>");
+
+        WebElement textXPath = getDriver().findElement(By.xpath("//input[@placeholder='Copy-paste your XPath expression here']"));
+        textXPath.sendKeys("//text()");
+
+        WebElement evaluateXPath = getDriver().findElement(By.id("formatBtn"));
+        evaluateXPath.click();
+
+        WebElement resultXPath = getDriver().findElement(By.xpath("//code[text()]"));
+
+        Assert.assertEquals(resultXPath.getText(), "Text='Harry Potter'");
+    }
+
+    @Test
+    public void testEtsyItemsOnSale() {
         getDriver().get("https://www.etsy.com/");
 
         WebElement search = getDriver().findElement(By.name("search_query"));
         search.sendKeys("easter gifts");
         search.sendKeys(Keys.RETURN);
 
-        WebElement filter = getDriver().findElement(By.id("search-filter-button"));
-        filter.click();
+        WebElement filterButton = getDriver().findElement(By.id("search-filter-button"));
+        filterButton.click();
 
-        WebElement onSale = getDriver().findElement(By.xpath("//label[@for='special-offers-on-sale']"));
-        onSale.click();
+        WebElement onSaleCheckbox = getDriver().findElement(By.xpath("//label[@for='special-offers-on-sale']"));
+        onSaleCheckbox.click();
 
-        WebElement apply = getDriver().findElement(By.xpath("//button[@aria-label='Apply']"));
-        apply.click();
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(1000));
 
-        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        WebElement applyButton = getDriver().findElement(By.xpath("//button[@aria-label='Apply']"));
+        applyButton.click();
+
+        getDriver().manage().timeouts().implicitlyWait(Duration.ofMillis(2000));
 
         WebElement text = getDriver().findElement(By.xpath("//a[@aria-label='Remove On sale Filter']"));
 
         Assert.assertEquals(text.getText(), "On sale");
-    }
-
-    @Test
-    public void testJenkinsTitle() {
-        getDriver().get("https://www.jenkins.io/");
-
-        Assert.assertEquals(getDriver().getTitle(),"Jenkins");
     }
 }

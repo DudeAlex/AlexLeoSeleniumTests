aabe7adb08f10a58d724c3915af140d643f37366
Nikolay <nikolay.zanin@gmail.com>
2023-04-20 21:52:18 +0700

    Add simple test for learning

--- src/test/java/school/redrover/ComradesAdelanteTest.java
+++ src/test/java/school/redrover/ComradesAdelanteTest.java
@@ -215,5 +215,24 @@ public class ComradesAdelanteTest extends BaseTest {
         Assert.assertEquals(actualResultSizeOfDeleteElementButton,expectResultSizeOfDeleteElementButton);
 
     }
-}
 
+    @Test
+    public void testSimpleDemoNickZanin() {
+
+            getDriver().get("https://demoqa.com/");
+
+            WebElement textElements = getDriver().findElement(By.xpath("//h5[normalize-space()='Elements']"));
+            textElements.click();
+
+            WebElement textCheckBox = getDriver().findElement(By.xpath("//span[normalize-space()='Check Box']"));
+            textCheckBox.click();
+
+            WebElement buttonExpandAll = getDriver().findElement(By.xpath("//button[@title='Expand all']"));
+            buttonExpandAll.click();
+
+            Assert.assertEquals("Notes", "Notes");
+
+            WebElement buttonCollapseAll = getDriver().findElement(By.xpath("//button[@title='Collapse all']"));
+            buttonCollapseAll.click();
+    }
+}

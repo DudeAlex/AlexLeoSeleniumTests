609790468be19c602d84020af582c0afd187d20e
janali713 <93846438+janali713@users.noreply.github.com>
2023-04-20 21:03:04 -0500

    Janali (#889)
    * Jana Li first commit
    
    * Jana Li first commit
    
    * Jana Li first commit
    
    ---------
    
    Co-authored-by: janalifanova <janalifanova@gmail.com>

--- src/test/java/school/redrover/DreamTeamTest.java
+++ src/test/java/school/redrover/DreamTeamTest.java
@@ -106,6 +106,18 @@ public class DreamTeamTest extends BaseTest {
         String value = message.getText();
         Assert.assertEquals("Received!", value);
     }
+
+    @Test
+    public void testCheckLinkPeoplePresent()  {
+
+        getDriver().get("https://jenkins.openbmc.org/");
+        WebElement linkPeople= getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a\n"));
+        linkPeople.click();
+        WebElement textPeople= getDriver().findElement(By.xpath("//*[@id='main-panel']/div[1]/div/h1"));
+        Assert.assertEquals(textPeople.getText(),"People");
+
+    }
+
 }
 
 

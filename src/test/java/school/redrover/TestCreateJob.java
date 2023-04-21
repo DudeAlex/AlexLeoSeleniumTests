8d4d3fbe5ad302012fc1929b30af5c49cca841f8
Oleh Yermolenko <57632851+eratheo@users.noreply.github.com>
2023-04-21 12:04:14 -0500

    Test case for creating new job

--- src/test/java/school/redrover/TestCreateJob.java
+++ src/test/java/school/redrover/TestCreateJob.java
@@ -1,9 +1,10 @@
-package school.redrover.runner;
+package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.runner.BaseTest;
 
 public class TestCreateJob extends BaseTest {
 

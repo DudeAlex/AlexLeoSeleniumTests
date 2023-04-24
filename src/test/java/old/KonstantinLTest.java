a74aef3a1d3effe4fd29ce7872eb7694cf14859a
Konstantin L <94020341+leokonss@users.noreply.github.com>
2023-04-23 22:34:17 -0500

    Kl/005 test (#134)
    * Add 2 more tests - TC_011, TC_012
    
    * Add new tests

--- src/test/java/old/KonstantinLTest.java
+++ src/test/java/old/KonstantinLTest.java
@@ -1,15 +1,18 @@
+package old;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import runner.BaseTest;
 
-import javax.crypto.spec.PSource;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+@Ignore
 public class KonstantinLTest extends BaseTest {
     @Test
     public void testSaleIconVerification() {
@@ -129,6 +132,112 @@ public class KonstantinLTest extends BaseTest {
             Assert.assertTrue(previous.compareTo(current) <= 0);
             previous = current;
         }
+    }
+
+    @Test
+    public void testNumberOfProductsInCartVerification() throws InterruptedException {
+        WebElement featuredProductsElement = getDriver().findElement(By.xpath("//h2[@class='has-text-align-center']"));
+        List<WebElement> featuredProducts = featuredProductsElement.findElements(By.xpath(
+                "//div[@class='astra-shop-thumbnail-wrap']/parent::li"));
+
+        int numItemsInCart = 0;
+        for (int i = 0; i < featuredProducts.size() - 1; i++) {
+            featuredProducts.get(i).findElement(By.xpath("//div[@class='astra-shop-summary-wrap']/a[contains(@href, " +
+                    "'add-to-cart')]")).click();
+            numItemsInCart++;
+        }
+        Thread.sleep(3000);
+
+        String expectedQuantity = getDriver().findElement(By.xpath("//div[@class='ast-cart-menu-wrap']/span")).getText();
+        Assert.assertEquals(Integer.parseInt(expectedQuantity), numItemsInCart);
+    }
+
+    @Test
+    public void testDiscountedPriceVerification() {
+        getDriver().findElement(By.xpath("//li[@id='menu-item-1230']/a[@href='https://askomdch.com/product-category/" +
+                "accessories/']")).click();
+        List<WebElement> elements = getDriver().findElements(By.xpath("//div[@id='woocommerce_top_rated_products-3']/" +
+                "ul/li"));
+
+        for (WebElement element : elements) {
+            String isDiscounted = "false";
+            try {
+                isDiscounted = element.findElement(By.cssSelector("li > del[aria-hidden*='true']"))
+                        .getAttribute("aria-hidden");
+            } catch (Exception e) {}
+
+            if (isDiscounted.equals("true")) {
+                double discountedPrice = Double.parseDouble(element.findElement(By.cssSelector("ins > span > bdi"))
+                        .getText().substring(1));
+                double price = Double.parseDouble(element.findElement(By.cssSelector("del > span > bdi"))
+                        .getText().substring(1));
+                Assert.assertTrue(discountedPrice < price);
+            }
+        }
+    }
+
+    @Test
+    public void testAddedProductsOver33Verification() throws InterruptedException {
+        getDriver().findElement(By.id("menu-item-1227")).click();
+        WebElement products = getListOfProducts(getDriver());
+        List<WebElement> productList = products.findElements(By.cssSelector("li"));
+
+        int numItemsInCart = 0;
+        for (WebElement element : productList) {
+            String isDiscounted = "false";
+            try {
+                isDiscounted = element.findElement(By.cssSelector("del[aria-hidden*='true']"))
+                        .getAttribute("aria-hidden");
+            } catch (Exception e) {}
+
+            double price = 0;
+            if (isDiscounted.equals("true")) {
+                price = Double.parseDouble(element.findElement(By.cssSelector("ins > span > bdi"))
+                        .getText().substring(1));
+            } else {
+                price = Double.parseDouble(element.findElement(By.cssSelector("div > span > span > bdi"))
+                        .getText().substring(1));
+            }
+
+            if (price > 33) {
+                element.findElement(By.cssSelector("a[href*='?add-to-cart']")).click();
+                numItemsInCart++;
+            }
+        }
+        Thread.sleep(3000);
+
+        String expectedQuantity = getDriver().findElement(By.xpath("//div[@class='ast-cart-menu-wrap']/span")).getText();
+        Assert.assertEquals(Integer.parseInt(expectedQuantity), numItemsInCart);
+    }
+
+    @Test
+    public void testDeletedProductsVerification() throws InterruptedException {
+        List<WebElement> featuredProducts = getDriver().findElements(By.xpath("//div[@class='woocommerce columns-5 ']/ul/li"));
+
+        int numItemsInCart = 0;
+        for (WebElement element : featuredProducts) {
+            String isDiscounted = "false";
+            try {
+                isDiscounted = element.findElement(By.cssSelector("del[aria-hidden*='true']"))
+                        .getAttribute("aria-hidden");
+            } catch (Exception e) {}
+
+            if (isDiscounted.equals("true")) {
+                element.findElement(By.cssSelector("a[href*='?add-to-cart']")).click();
+                numItemsInCart++;
+            }
+        }
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//div[@class='ast-main-header-wrap main-header-bar-wrap ']//span")).click();
+
+        for (int i = 0; i < numItemsInCart; i++) {
+            getDriver().findElement(By.xpath("//td[@class='product-remove']/a[contains(@aria-label, 'Remove this item')]")).click();
+            Thread.sleep(1500);
+        }
+
+        String emptyCart = getDriver().findElement(By.xpath("//p[@class='cart-empty woocommerce-info']")).getText();
+        String expectedString = "Your cart is currently empty.";
+        Assert.assertEquals(emptyCart, expectedString);
     }
 
     private List<Double> getPrices(WebElement products) {

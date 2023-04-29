bad059ec15a233aed8b2990f0b7acf0ea60c863b
ktitova <ksunchik123@yandex.ru>
2023-04-29 10:10:22 +0400

    test added testEnableProject

--- src/test/java/FreestyleProjectTest.java
+++ src/test/java/FreestyleProjectTest.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -5,31 +6,51 @@ import runner.BaseTest;
 
 public class FreestyleProjectTest extends BaseTest {
 
+    private static final String FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
+    private static final By GO_TO_DASHBOARD_BUTTON = By.linkText("Dashboard");
     @Test
     public void testCreateNewFreestyleProject() {
 
         getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys("First project");
+        getDriver().findElement(By.id("name")).sendKeys(FREESTYLE_NAME);
         getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.cssSelector("#ok-button")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
 
         Assert.assertEquals(getDriver()
-                .findElement(By.xpath("//h1")).getText(), "Project " + "First project");
+                .findElement(By.xpath("//h1")).getText(), "Project " + FREESTYLE_NAME);
     }
 
     @Test
     public void testDisableProject() {
 
         getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys("First project");
+        getDriver().findElement(By.id("name")).sendKeys(FREESTYLE_NAME);
         getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.cssSelector("#ok-button")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + "First project");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Project " + FREESTYLE_NAME);
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'warning']")).getText().trim().substring(0, 34),
                 "This project is currently disabled");
     }
+
+
+    @Test
+    public void testEnableProject() {
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(FREESTYLE_NAME);
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+
+        getDriver().findElement(By.cssSelector("button.jenkins-button.jenkins-button--primary ")).click();
+        getDriver().findElement(GO_TO_DASHBOARD_BUTTON).click();
+
+        Assert.assertEquals(getDriver().findElement(
+                By.xpath("//span/span/*[name()='svg' and @class= 'svg-icon ']")).getAttribute("tooltip"), "Not built");
+    }
+
 }

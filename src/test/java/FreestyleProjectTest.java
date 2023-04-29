21443c979f529066b7b204c21a7687cc14bbb35b
ktitova <ksunchik123@yandex.ru>
2023-04-29 13:14:12 +0400

    testRenameFreestyleProject

--- src/test/java/FreestyleProjectTest.java
+++ src/test/java/FreestyleProjectTest.java
@@ -1,3 +1,4 @@
+import net.bytebuddy.description.type.TypeList;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.testng.Assert;
@@ -8,6 +9,7 @@ public class FreestyleProjectTest extends BaseTest {
 
     private static final String FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
     private static final By GO_TO_DASHBOARD_BUTTON = By.linkText("Dashboard");
+    private static final String NEW_FREESTYLE_NAME = RandomStringUtils.randomAlphanumeric(10);
 
     @Test
     public void testCreateNewFreestyleProject() {
@@ -93,6 +95,24 @@ public class FreestyleProjectTest extends BaseTest {
         getDriver().findElement(By.cssSelector("button.jenkins-button.jenkins-button--primary")).click();
 
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id = 'description'] /div[1]")).getText(), "Job " + FREESTYLE_NAME);
+    }
+
+    @Test
+    public void testRenameFreestyleProject() {
+
+        getDriver().findElement(By.linkText("New Item")).click();
+        getDriver().findElement(By.id("name")).sendKeys(FREESTYLE_NAME);
+        getDriver().findElement(By.cssSelector(".hudson_model_FreeStyleProject")).click();
+        getDriver().findElement(By.cssSelector("#ok-button")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate = 'formNoValidate']")).click();
+
+        getDriver().findElement(By.xpath("//a[@href='/job/" + FREESTYLE_NAME + "/confirm-rename']")).click();
+        getDriver().findElement(By.cssSelector("input[name='newName']")).clear();
+        getDriver().findElement(By.cssSelector("input[name='newName']")).sendKeys(NEW_FREESTYLE_NAME);
+        getDriver().findElement(By.xpath("//button[@formnovalidate]")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.cssSelector("h1.job-index-headline.page-headline")).getText(),
+                "Project " + NEW_FREESTYLE_NAME);
     }
 
 }

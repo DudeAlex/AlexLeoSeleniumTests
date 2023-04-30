2a42d54d2265a464ed41ebe0150d7578de1aff1a
Evan Mai <mai.evan@yahoo.com>
2023-04-30 12:05:00 -0700

    new test

--- src/test/java/UserPageTest.java
+++ src/test/java/UserPageTest.java
@@ -1,3 +1,4 @@
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +13,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class UserPageTest extends BaseTest {
+
+    private static final String USER_FULL_NAME = RandomStringUtils.randomAlphanumeric(10);
 
     private WebDriverWait getWait() {
         return new WebDriverWait(getDriver(), Duration.ofSeconds(3));
@@ -29,5 +32,20 @@ public class UserPageTest extends BaseTest {
         for (int i = 0; i < listMenu.size(); i++) {
             Assert.assertEquals(listMenu.get(i).getText(), listMenuExpected.get(i));
         }
+    }
+
+    @Test
+    public void testVerifyChangeNameUser() {
+        getDriver().findElement(By.xpath("//a[@class='model-link']")).click();
+
+        WebElement configure = getWait().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/user/admin/configure']")));
+        configure.click();
+
+        WebElement fullName = getWait().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='_.fullName']")));
+        fullName.clear();
+        fullName.sendKeys(USER_FULL_NAME);
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).click();
+
+        Assert.assertEquals(getDriver().findElement(By.xpath("//a[@href='/user/admin']")).getText(), USER_FULL_NAME);
     }
 }

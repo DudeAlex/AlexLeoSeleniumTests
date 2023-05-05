a3c312bb3b56e9d5117ee0e43b1cd6b88d611c0a
Evan Mai <mai.evan@yahoo.com>
2023-05-05 16:41:57 -0700

    refactoring

--- src/test/java/AlexLeoGroupJenkins06Tests.java
+++ src/test/java/AlexLeoGroupJenkins06Tests.java
@@ -22,7 +22,7 @@ public class AlexLeoGroupJenkins06Tests extends BaseTest {
                 .findElement(By.id("jenkins-name-icon"));
        Assert.assertTrue(logoWord.isDisplayed());
     }
-;    @Test
+    @Test
     public void testIconVerification(){
         WebElement icon=getDriver().findElement(By.xpath("//h1[text()='Welcome to Jenkins!']"));
         Assert.assertEquals(icon.getText(),"Welcome to Jenkins!");

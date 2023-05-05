0e8ebb92b6e40273162f002ad23e6ab4490ec338
Evan Mai <mai.evan@yahoo.com>
2023-05-05 16:33:46 -0700

    refactoring

--- src/test/java/AlexLeoGroupJenkins06Tests.java
+++ src/test/java/AlexLeoGroupJenkins06Tests.java
@@ -1,8 +1,11 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import runner.BaseTest;
+
+import java.util.List;
 
 
 public class AlexLeoGroupJenkins06Tests extends BaseTest {
@@ -28,5 +31,24 @@ public class AlexLeoGroupJenkins06Tests extends BaseTest {
     @Test
     public void testVerifyDRPMenu() {
         Assert.assertEquals(getDriver().findElement(By.xpath("//div[@id='breadcrumbBar']/ol/li/a")).getText(), "Dashboard");
+    }
+
+
+    @Test
+    public void testManageOldData() {
+        getDriver().findElement(By.xpath("//a[@href='/manage']")).click();
+
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//dt[contains(text(),'Manage Old Data')]"))).click();
+
+        WebElement oldData = getWait5().until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#main-panel > h1")));
+        Assert.assertEquals(oldData.getText(), "Manage Old Data");
+        Assert.assertEquals(oldData.getLocation().toString(), "(372, 133)");
+        Assert.assertEquals(oldData.getCssValue("font-size").toString(), "25.6px");
+        Assert.assertEquals(oldData.getCssValue("font-weight").toString(), "700");
+
+        List<WebElement> listSortTable = getDriver().findElements(By.xpath("//thead //a"));
+        Assert.assertEquals(listSortTable.size(), 4);
+
+        Assert.assertTrue(getDriver().findElement(By.id("main-panel")).getText().contains("No old data was found."));
     }
 }
